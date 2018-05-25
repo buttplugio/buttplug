23d6ee2bb2854133fd23aba6fd88b53e4353cb81
Kyle Machulis <kyle@nonpolynomial.com>
2018-05-25 12:02:40 -0700

    Move to using branch of mermaid plugin with config options
    Allows configuring layout of diagrams, so we can make the lifecycle
    diagram wider.

--- spec/book.js
+++ spec/book.js
@@ -1,5 +1,15 @@
-module.exports = { "root": "./protocol-spec" };
-module.exports["plugins"] = ["mermaid-gb3"];
+module.exports = {
+  "root": "./protocol-spec",
+  "plugins": [],
+  "pluginsConfig": {}
+};
+
+module.exports["plugins"].push("mermaid-gb3");
+module.exports["pluginsConfig"]["mermaid-gb3"] = {
+  "sequenceDiagram": {
+    "actorMargin": 200
+  }
+};
 
 // Only add piwik if we're building on the CI and deploying
 if (process.env.CI) {
