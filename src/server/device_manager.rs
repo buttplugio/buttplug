23b7d2b38660ea4aa5e6a420ad6e511d168f62e9
Kyle Machulis <kyle@nonpolynomial.com>
2019-10-05 16:24:42 -0700

    chore: Add DeviceManager stub

--- src/server/device_manager.rs
+++ src/server/device_manager.rs
@@ -1,3 +1,11 @@
+use crate::core::errors::ButtplugError;
+
+trait DeviceSubtypeManager {
+    fn start_scanning() -> Result<(), ButtplugError>;
+    fn stop_scanning() -> Result<(), ButtplugError>;
+    fn is_scanning() -> bool;
+}
 
 struct DeviceManager {
+    
 }
