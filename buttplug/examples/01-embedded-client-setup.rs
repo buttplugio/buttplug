5b6545fa612e41a79721c44a5206dc35d437d76f
blackspherefollower <darktech@black-sphere.co.uk>
2020-01-17 15:22:10 +0000

    feat: PoC end-to-end embedded client

--- buttplug/examples/01-embedded-client-setup.rs
+++ buttplug/examples/01-embedded-client-setup.rs
@@ -16,13 +16,17 @@
 // multiple packages for different features, the Rust Buttplug crate contains
 // everything you need to build Buttplug applications. Aren't Cargo Features
 // great?
-use buttplug::client::{connectors::ButtplugEmbeddedClientConnector, ButtplugClient};
+use buttplug::client::{connectors::ButtplugEmbeddedClientConnector, ButtplugClient, ButtplugClientEvent};
 
 // We're gonna use async_std as our runtime for the examples, but you should be
 // able to use futures, tokio, or whatever else.
 use async_std::task;
+use buttplug::server::comm_managers::btleplug::BtlePlugCommunicationManager;
+use std::time::Duration;
+use buttplug::client::device::VibrateCommand;
 
 async fn embedded_connector_example() {
+    env_logger::init();
     // We'll need a connector first, as creating a client requires a connector.
     // Connectors are how clients connect to servers. Since we're just starting
     // out and don't want to deal with networks or IPC yet, we'll create an
@@ -32,8 +36,8 @@ async fn embedded_connector_example() {
     //
     // For now, we'll just give the server a name. We'll go over other server
     // constructor arguments in later examples.
-    let connector = ButtplugEmbeddedClientConnector::new("Example Server", 0);
-
+    let mut connector = ButtplugEmbeddedClientConnector::new("Example Server", 0);
+    connector.add_comm_manager::<BtlePlugCommunicationManager>();
     // Now that we've got a connector, we can use the ButtplugClient::run()
     // function to spin up our client event loop. We pass this function three
     // things:
@@ -62,15 +66,135 @@ async fn embedded_connector_example() {
     // Note that the closure is (ButtplugClient) -> impl Future. We'll explain
     // why in the definition.
     let app_closure = |mut client: ButtplugClient| {
-        // Things get a little weird in here, as we need to
-        // return a Future from our closure for the event
-        // loop to run. Inside this async block is where
-        // we'll usually put our Buttplug application code.
         async move {
-            // We'll just have the client disconnect itself.
-            // Since we know we're connected if we've gotten
-            // this far, we can just unwrap here.
-            client.disconnect().await.unwrap();
+            // First, we'll start the server looking for devices.
+            if let Err(err) = client.start_scanning().await {
+                // If the server disconnected between the time we spun up the
+                // loop and now, the scanning will return an error. At that
+                // point we should just bail out.
+                println!("Client errored when starting scan! {}", err);
+                return;
+            }
+            // Ok, we've started scanning. Now we need to wait to hear back from
+            // the server on whether we got anything. To do that, we call
+            // wait_for_event.
+            //
+            // wait_for_event is to Buttplug's Rust implementation what the
+            // event handlers in C#/JS were to those implementations. However,
+            // since we're not in a GC'd language anymore, event handlers are a
+            // bit difficult to implement, so we just have a stream-like
+            // function instead.
+            //
+            // wait_for_event will return a future that waits until it gets
+            // something from the server. You can either await that and block
+            // until you get something from the server (or race/select it
+            // against other futures), or else save the future and use something
+            // like a timeout join.
+            //
+            // For our purposes for the moment, all we care about is receiving
+            // new devices, so we'll just loop and wait.
+            let mut device = None;
+            loop {
+                match client.wait_for_event().await {
+                    // Yay we got an event!
+                    Ok(event) => match event {
+                        ButtplugClientEvent::DeviceAdded(dev) => {
+                            // And we actually got a device!
+                            //
+                            // The device we're given is a real
+                            // ButtplugClientDevice object. We could control the
+                            // device with it if we wanted, but that's coming up
+                            // in a later example. For now, we'll just print the
+                            // device name then drop our instance of it.
+                            println!("We got a device: {}", dev.name);
+                            device = Some(dev);
+                            break;
+                        }
+                        ButtplugClientEvent::ServerDisconnect => {
+                            // The server disconnected, which means we're done
+                            // here, so just break up to the top level.
+                            println!("Server disconnected!");
+                            break;
+                        }
+                        _ => {
+                            // Something else happened, like scanning finishing,
+                            // devices getting removed, etc... Might as well say
+                            // something about it.
+                            println!("Got some other kind of event we don't care about");
+                        }
+                    },
+                    // Once again, if we disconnected before calling
+                    // wait_for_error, we'll get an error back.
+                    Err(err) => {
+                        println!("Error while waiting for client events: {}", err);
+                        break;
+                    }
+                }
+            }
+
+            // Ok, so we now have a connected client with a device set up. Let's
+            // start sending some messages to make the device do things!
+            //
+            // It's worth noting that at the moment, a client knowing about a
+            // device is enough to assume that device is connected to the server
+            // and ready to use. So if a client has a device in its list, we can
+            // just start sending control messages.
+            if let Some(mut dev) = device {
+                // We'll need to see which messages our device handles. Luckily,
+                // devices hold this information for you to query.
+                //
+                // When building applications, we can use allowed_messages to
+                // see what types of messages whatever device handed to us can
+                // take, and then react accordingly.
+                //
+                // Each entry of allowed_messages will have two pieces of
+                // information
+                //
+                // - Message Type, which will represent the classes of messages
+                // we can send
+                //
+                // - Message Attributes, which can vary depending on the type
+                // of message
+                //
+                // For instance the VibrateCmd message will have a name of
+                // "VibrateCmd", and a "FeatureCount" of 1 < x < N, depending on
+                // the number of vibration motors the device has. Messages that
+                // don't have a FeatureCount will leave Option<FeatureCount> as
+                // None.
+                //
+                // Since we don't know what kind of device we'll be getting
+                // here, we just assume it will be something that vibrates.
+                //
+                // Devices have "generic" commands for vibrate, rotate, and
+                // linear (movement). Each of these takes a enum that is either:
+                //
+                // - A single value to send to all features. For instance if a
+                // device has 6 vibrators, and we send one speed, all 6
+                // vibrators will be set to that speed.
+                //
+                // - A map of index/value pairs, which allows setting certain
+                // device feature indexes to certain values.
+                //
+                // - A vector of values, which can address most or all feature
+                // indexes.
+                //
+                // For this example, we'll use the simple single value.
+
+                if dev.allowed_messages.contains_key("VibrateCmd") {
+                    dev.vibrate(VibrateCommand::Speed(1.0)).await.unwrap();
+                    println!("{} should start vibrating!", dev.name);/*
+                    task::sleep(Duration::from_secs(1)).await;
+                    // All devices also have a "stop" command that will make
+                    // them stop whatever they're doing.
+                    dev.stop().await.unwrap();
+                    println!("{} should stop vibrating!", dev.name);
+                    task::sleep(Duration::from_secs(1)).await;*/
+                } else {
+                    println!("{} doesn't vibrate! This example should be updated to handle rotation and linear movement!", dev.name);
+                }
+            }
+            // And now we're done!
+            println!("Exiting example");
         }
     };
     ButtplugClient::run("Example Client", connector, app_closure)
