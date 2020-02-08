ec67eb7be4fe01f67aac00c7c13afe095e3e0597
Kyle Machulis <kyle@nonpolynomial.com>
2020-02-07 21:35:34 -0800

    feat: Add macro generation for protocol classes
    Use a macro to create the ProtocolCreator struct and impl
    ButtplugProtocol for protocols. Use for Aneros and KiirooGen2 (because
    they were the first 2 in the list).

--- buttplug/src/device/protocol/aneros.rs
+++ buttplug/src/device/protocol/aneros.rs
@@ -1,5 +1,6 @@
 use super::{ButtplugProtocol, ButtplugProtocolCreator};
 use crate::{
+    create_buttplug_protocol_impl,
     core::{
         errors::{ButtplugDeviceError, ButtplugError},
         messages::{
@@ -14,28 +15,6 @@ use crate::{
     },
 };
 use async_trait::async_trait;
-
-pub struct AnerosProtocolCreator {
-    config: DeviceProtocolConfiguration,
-}
-
-impl AnerosProtocolCreator {
-    pub fn new(config: DeviceProtocolConfiguration) -> Self {
-        Self { config }
-    }
-}
-
-#[async_trait]
-impl ButtplugProtocolCreator for AnerosProtocolCreator {
-    async fn try_create_protocol(
-        &self,
-        device_impl: &Box<dyn DeviceImpl>,
-    ) -> Result<Box<dyn ButtplugProtocol>, ButtplugError> {
-        let (names, attrs) = self.config.get_attributes(device_impl.name()).unwrap();
-        let name = names.get("en-us").unwrap();
-        Ok(Box::new(AnerosProtocol::new(name, attrs)))
-    }
-}
 
 #[derive(Clone)]
 pub struct AnerosProtocol {
@@ -62,38 +41,9 @@ impl AnerosProtocol {
     }
 }
 
-#[async_trait]
-impl ButtplugProtocol for AnerosProtocol {
-    fn name(&self) -> &str {
-        &self.name
-    }
-
-    fn message_attributes(&self) -> MessageAttributesMap {
-        self.attributes.clone()
-    }
-
-    fn box_clone(&self) -> Box<dyn ButtplugProtocol> {
-        Box::new((*self).clone())
-    }
-
-    async fn parse_message(
-        &mut self,
-        device: &Box<dyn DeviceImpl>,
-        message: &ButtplugDeviceCommandMessageUnion,
-    ) -> Result<ButtplugMessageUnion, ButtplugError> {
-        match message {
-            ButtplugDeviceCommandMessageUnion::StopDeviceCmd(msg) => {
-                self.handle_stop_device_cmd(device, msg).await
-            }
-            ButtplugDeviceCommandMessageUnion::VibrateCmd(msg) => {
-                self.handle_vibrate_cmd(device, msg).await
-            }
-            _ => Err(ButtplugError::ButtplugDeviceError(
-                ButtplugDeviceError::new("AnerosProtocol does not accept this message type."),
-            )),
-        }
-    }
-}
+create_buttplug_protocol_impl!(AnerosProtocol,
+                               (VibrateCmd, handle_vibrate_cmd),
+                               (StopDeviceCmd, handle_stop_device_cmd));
 
 impl AnerosProtocol {
     async fn handle_stop_device_cmd(
--- buttplug/src/device/protocol/mod.rs
+++ buttplug/src/device/protocol/mod.rs
@@ -9,9 +9,11 @@ pub mod youcups;
 pub mod youou;
 
 use super::device::DeviceImpl;
-use crate::core::{
-    errors::ButtplugError,
-    messages::{ButtplugDeviceCommandMessageUnion, ButtplugMessageUnion, MessageAttributesMap},
+use crate::{
+    core::{
+        errors::ButtplugError,
+        messages::{ButtplugDeviceCommandMessageUnion, ButtplugMessageUnion, MessageAttributesMap},
+    }
 };
 use async_trait::async_trait;
 
@@ -22,6 +24,75 @@ pub trait ButtplugProtocolCreator: Sync + Send {
         device_impl: &Box<dyn DeviceImpl>,
     ) -> Result<Box<dyn ButtplugProtocol>, ButtplugError>;
 }
+
+// Note: We have to use tt instead of ident here due to the async_trait macro.
+// See https://github.com/dtolnay/async-trait/issues/46 for more info.
+#[macro_export]
+macro_rules! create_buttplug_protocol_impl (
+    (
+        $protocol_name:tt,
+        $(
+            ( $message_name:tt, $message_handler:tt )
+        ),+
+    ) => {
+        paste::item! {
+            pub struct [<$protocol_name Creator>] {
+                config: DeviceProtocolConfiguration,
+            }
+
+            impl [<$protocol_name Creator>] {
+                pub fn new(config: DeviceProtocolConfiguration) -> Self {
+                    Self { config }
+                }
+            }
+
+            #[async_trait]
+            impl ButtplugProtocolCreator for [<$protocol_name Creator>] {
+                async fn try_create_protocol(
+                    &self,
+                    device_impl: &Box<dyn DeviceImpl>,
+                ) -> Result<Box<dyn ButtplugProtocol>, ButtplugError> {
+                    let (names, attrs) = self.config.get_attributes(device_impl.name()).unwrap();
+                    let name = names.get("en-us").unwrap();
+                    Ok(Box::new($protocol_name::new(name, attrs)))
+                }
+            }
+
+        }
+
+        #[async_trait]
+        impl ButtplugProtocol for $protocol_name {
+            fn name(&self) -> &str {
+                &self.name
+            }
+
+            fn message_attributes(&self) -> MessageAttributesMap {
+                self.attributes.clone()
+            }
+
+            fn box_clone(&self) -> Box<dyn ButtplugProtocol> {
+                Box::new((*self).clone())
+            }
+
+            async fn parse_message(
+                &mut self,
+                device: &Box<dyn DeviceImpl>,
+                message: &ButtplugDeviceCommandMessageUnion,
+            ) -> Result<ButtplugMessageUnion, ButtplugError> {
+                match message {
+                    $(
+                        ButtplugDeviceCommandMessageUnion::$message_name(msg) => {
+                            self.$message_handler(device, msg).await
+                        }
+                    ),*
+                        _ => Err(ButtplugError::ButtplugDeviceError(
+                            ButtplugDeviceError::new("AnerosProtocol does not accept this message type."),
+                        )),
+                }
+            }
+        }
+    }
+);
 
 #[async_trait]
 pub trait ButtplugProtocol: Sync + Send {
