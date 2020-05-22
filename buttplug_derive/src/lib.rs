dfdad18624192822372c3e566fbd3a73933d5d24
Kyle Machulis <kyle@nonpolynomial.com>
2020-05-22 14:34:05 -0700

    chore: Remove unused use in buttplug_derive

--- buttplug_derive/src/lib.rs
+++ buttplug_derive/src/lib.rs
@@ -2,7 +2,6 @@ extern crate proc_macro;
 
 use proc_macro::TokenStream;
 use quote::quote;
-use syn;
 
 #[proc_macro_derive(ButtplugMessage)]
 pub fn buttplug_message_derive(input: TokenStream) -> TokenStream {
