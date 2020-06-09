91cd3999efb78bd768cce1105279cb7d7fc22ecd
Kyle Machulis <kyle@nonpolynomial.com>
2020-06-08 21:58:06 -0700

    chore: Fix runtime feature selection
    Use cfg-if to select our runtime.

--- buttplug/src/util/async_manager/mod.rs
+++ buttplug/src/util/async_manager/mod.rs
@@ -1,49 +1,17 @@
-#[cfg(feature = "thread_pool_runtime")]
-mod thread_pool;
-#[cfg(feature="dummy_runtime")]
-mod dummy;
-#[cfg(feature="async_std_runtime")]
-mod async_std;
-
-#[cfg(feature = "thread_pool_runtime")]
-pub use thread_pool::{ThreadPoolAsyncManager as AsyncManager, spawn, spawn_with_handle, block_on};
-#[cfg(feature="dummy_runtime")]
-pub use dummy::{DummyAsyncManager as AsyncManager, spawn, spawn_with_handle, block_on};
-#[cfg(feature="async_std_runtime")]
-pub use self::async_std::{AsyncStdAsyncManager as AsyncManager, spawn, spawn_with_handle, block_on};
-
-/*
-use futures::{
-  future::{Future, RemoteHandle},
-  task::{FutureObj, Spawn, SpawnError, SpawnExt},
-};
-
-#[cfg(feature = "thread_pool_runtime")]
-//pub use thread_pool::{ThreadPoolAsyncManager as AsyncManager, spawn, spawn_with_handle, block_on};
-#[cfg(feature="dummy_runtime")]
-pub use dummy::{DummyAsyncManager as AsyncManager, spawn, spawn_with_handle, block_on};
-#[cfg(feature="async_std_runtime")]
-pub use async_executors::AsyncStd as AsyncManager;
-
-pub fn spawn<Fut>(future: Fut) -> Result<(), SpawnError>
-where
-  Fut: Future<Output = ()> + Send + 'static,
-{
-  AsyncManager::default().spawn(future)
+cfg_if::cfg_if! {
+  if #[cfg(feature = "thread_pool_runtime")] {
+    mod thread_pool;
+    pub use thread_pool::{ThreadPoolAsyncManager as AsyncManager, spawn, spawn_with_handle, block_on};
+  } else if #[cfg(feature = "dummy_runtime")] {
+    mod dummy;
+    pub use dummy::{DummyAsyncManager as AsyncManager, spawn, spawn_with_handle, block_on};
+  } else if #[cfg(feature = "async_std_runtime")] {
+    mod async_std;
+    pub use self::async_std::{AsyncStdAsyncManager as AsyncManager, spawn, spawn_with_handle, block_on};
+  } else if #[cfg(feature = "wasm_bindgen_runtime")] {
+    mod wasm_bindgen;
+    pub use wasm_bindgen::{WasmBindgenAsyncManager as AsyncManager, spawn, spawn_with_handle, block_on};
+  } else {
+    std::compile_error!("Please choose a runtime feature: thread_pool_runtime, async_std_runtime, wasm_bindgen_runtime, dummy_runtime");
+  }
 }
-
-pub fn spawn_with_handle<Fut>(future: Fut) -> Result<RemoteHandle<Fut::Output>, SpawnError>
-where
-  Fut: Future + Send + 'static,
-  Fut::Output: Send,
-{
-  AsyncManager::default().spawn_with_handle(future)
-}
-
-pub fn block_on<F>(f: F) -> <F as Future>::Output
-where
-  F: Future,
-{
-  block_on_executor(f)
-}
-*/
