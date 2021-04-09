a55255d4de7050e68e7e0fd1d104c39a467762ab
Kyle Machulis <kyle@nonpolynomial.com>
2021-04-08 18:40:25 -0700

    chore: Remove futures::ThreadPool executor
    Never really used it, shouldn't have been default, probably
    confused users.
    
    Affects #311

--- buttplug/src/util/async_manager/mod.rs
+++ buttplug/src/util/async_manager/mod.rs
@@ -1,8 +1,5 @@
 cfg_if::cfg_if! {
-  if #[cfg(feature = "thread-pool-runtime")] {
-    mod thread_pool;
-    pub use thread_pool::{ThreadPoolAsyncManager as AsyncManager, spawn, spawn_with_handle, block_on};
-  } else if #[cfg(feature = "dummy-runtime")] {
+  if #[cfg(feature = "dummy-runtime")] {
     mod dummy;
     pub use dummy::{DummyAsyncManager as AsyncManager, spawn, spawn_with_handle, block_on};
   } else if #[cfg(feature = "async-std-runtime")] {
