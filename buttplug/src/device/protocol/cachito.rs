71a0285e87ba2e7508e4099682264d875584cc59
blackspherefollower <darktech@black-sphere.co.uk>
2021-04-30 08:35:36 -0700

    feat: Adding support for 2 Cachito devices
    Both have heavy solenoid based thrusting oscillators.
    The Tao has a suction oscillator
    The Ice Cream has a vibration motor.

---
+++ buttplug/src/device/protocol/cachito.rs
@@ -0,0 +1,163 @@
+use super::{ButtplugDeviceResultFuture, ButtplugProtocol, ButtplugProtocolCommandHandler};
+use crate::{
+  core::messages::{self, ButtplugDeviceCommandMessageUnion, DeviceMessageAttributesMap},
+  device::{
+    protocol::{generic_command_manager::GenericCommandManager, ButtplugProtocolProperties},
+    DeviceImpl, DeviceWriteCmd, Endpoint,
+  },
+};
+use std::sync::Arc;
+use tokio::sync::Mutex;
+
+#[derive(ButtplugProtocolProperties)]
+pub struct Cachito {
+  name: String,
+  message_attributes: DeviceMessageAttributesMap,
+  manager: Arc<Mutex<GenericCommandManager>>,
+  stop_commands: Vec<ButtplugDeviceCommandMessageUnion>,
+}
+
+impl ButtplugProtocol for Cachito {
+  fn new_protocol(
+    name: &str,
+    message_attributes: DeviceMessageAttributesMap,
+  ) -> Box<dyn ButtplugProtocol>
+  where
+    Self: Sized,
+  {
+    let manager = GenericCommandManager::new(&message_attributes);
+
+    Box::new(Self {
+      name: name.to_owned(),
+      message_attributes,
+      stop_commands: manager.get_stop_commands(),
+      manager: Arc::new(Mutex::new(manager)),
+    })
+  }
+}
+
+impl ButtplugProtocolCommandHandler for Cachito {
+  fn handle_vibrate_cmd(
+    &self,
+    device: Arc<DeviceImpl>,
+    message: messages::VibrateCmd,
+  ) -> ButtplugDeviceResultFuture {
+    // Store off result before the match, so we drop the lock ASAP.
+    let manager = self.manager.clone();
+    Box::pin(async move {
+      let result = manager.lock().await.update_vibration(&message, false)?;
+      let mut fut_vec = vec![];
+      if let Some(cmds) = result {
+        for (index, cmd) in cmds.iter().enumerate() {
+          if let Some(speed) = cmd {
+            fut_vec.push(device.write_value(DeviceWriteCmd::new(
+              Endpoint::Tx,
+              vec![2u8 + (index as u8), 1u8 + (index as u8), *speed as u8, 0u8],
+              false,
+            )));
+          }
+        }
+      }
+      // TODO Just use join_all here
+      for fut in fut_vec {
+        // TODO Do something about possible errors here
+        fut.await?;
+      }
+      Ok(messages::Ok::default().into())
+    })
+  }
+}
+
+#[cfg(all(test, feature = "server"))]
+mod test {
+  use crate::{
+    core::messages::{StopDeviceCmd, VibrateCmd, VibrateSubcommand},
+    device::{DeviceImplCommand, DeviceWriteCmd, Endpoint},
+    test::{check_test_recv_empty, check_test_recv_value, new_bluetoothle_test_device},
+    util::async_manager,
+  };
+
+  #[test]
+  pub fn test_cachito_protocol() {
+    async_manager::block_on(async move {
+      let (device, test_device) = new_bluetoothle_test_device("CCTSK").await.unwrap();
+      let command_receiver = test_device.get_endpoint_receiver(&Endpoint::Tx).unwrap();
+      device
+        .parse_message(VibrateCmd::new(0, vec![VibrateSubcommand::new(0, 0.5)]).into())
+        .await
+        .unwrap();
+      // We just vibe 1 so expect 1 write
+      check_test_recv_value(
+        &command_receiver,
+        DeviceImplCommand::Write(DeviceWriteCmd::new(Endpoint::Tx, vec![2, 1, 3, 0], false)),
+      );
+      assert!(check_test_recv_empty(&command_receiver));
+
+      device
+        .parse_message(VibrateCmd::new(0, vec![VibrateSubcommand::new(0, 0.5)]).into())
+        .await
+        .unwrap();
+      // no-op
+      assert!(check_test_recv_empty(&command_receiver));
+
+      device
+        .parse_message(
+          VibrateCmd::new(
+            0,
+            vec![
+              VibrateSubcommand::new(0, 0.1),
+              VibrateSubcommand::new(1, 0.5),
+            ],
+          )
+          .into(),
+        )
+        .await
+        .unwrap();
+      // fianlly setting second vibe whilst changing vibe 1, 2 writes
+      check_test_recv_value(
+        &command_receiver,
+        DeviceImplCommand::Write(DeviceWriteCmd::new(Endpoint::Tx, vec![2, 1, 1, 0], false)),
+      );
+      check_test_recv_value(
+        &command_receiver,
+        DeviceImplCommand::Write(DeviceWriteCmd::new(Endpoint::Tx, vec![3, 2, 50, 0], false)),
+      );
+      assert!(check_test_recv_empty(&command_receiver));
+
+      device
+          .parse_message(
+            VibrateCmd::new(
+              0,
+              vec![
+                VibrateSubcommand::new(0, 0.1),
+                VibrateSubcommand::new(1, 0.9),
+              ],
+            )
+                .into(),
+          )
+          .await
+          .unwrap();
+      // only vibe 1 changed
+      check_test_recv_value(
+        &command_receiver,
+        DeviceImplCommand::Write(DeviceWriteCmd::new(Endpoint::Tx, vec![3, 2, 90, 0], false)),
+      );
+      assert!(check_test_recv_empty(&command_receiver));
+
+      device
+        .parse_message(StopDeviceCmd::new(0).into())
+        .await
+        .unwrap();
+      // stop on both
+      check_test_recv_value(
+        &command_receiver,
+        DeviceImplCommand::Write(DeviceWriteCmd::new(Endpoint::Tx, vec![2, 1, 0, 0], false)),
+      );
+      check_test_recv_value(
+        &command_receiver,
+        DeviceImplCommand::Write(DeviceWriteCmd::new(Endpoint::Tx, vec![3, 2, 0, 0], false)),
+      );
+      assert!(check_test_recv_empty(&command_receiver));
+    });
+  }
+}
