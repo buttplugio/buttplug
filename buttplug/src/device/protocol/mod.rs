395a8512f27e34862fcc6954dc0f22f78276fecb
blackspherefollower <darktech@black-sphere.co.uk>
2021-05-07 07:01:40 -0700

    feat: Adding support for Je Joue Nuo and Dua
    Only tested with the Dua.
    
    Since this device takes 1 speed for neither or both motors,
    the first vibe speed is used to control both motors. If only
    1 of the 2 vibes has non-zero speeds, that vibe will run at
    the requested speed.

--- buttplug/src/device/protocol/mod.rs
+++ buttplug/src/device/protocol/mod.rs
@@ -3,6 +3,7 @@ pub mod aneros;
 pub mod cachito;
 pub mod fleshlight_launch_helper;
 pub mod generic_command_manager;
+pub mod jejoue;
 pub mod kiiroo_v2;
 pub mod kiiroo_v21;
 pub mod kiiroo_v21_initialized;
@@ -63,6 +64,7 @@ pub fn get_default_protocol_map() -> DashMap<String, TryCreateProtocolFunc> {
   let map = DashMap::new();
   add_to_protocol_map::<aneros::Aneros>(&map, "aneros");
   add_to_protocol_map::<cachito::Cachito>(&map, "cachito");
+  add_to_protocol_map::<jejoue::JeJoue>(&map, "jejoue");
   add_to_protocol_map::<kiiroo_v2::KiirooV2>(&map, "kiiroo-v2");
   add_to_protocol_map::<kiiroo_v2_vibrator::KiirooV2Vibrator>(&map, "kiiroo-v2-vibrator");
   add_to_protocol_map::<kiiroo_v21::KiirooV21>(&map, "kiiroo-v21");
