ab59b76d266121a54e223db6270a891cff0dfc19
blackspherefollower <darktech@black-sphere.co.uk>
2021-05-09 03:49:20 -0700

    feat: Lovenuts support

---
+++ buttplug/src/device/protocol/lovenuts.rs
@@ -0,0 +1,144 @@
+use super::{ButtplugDeviceResultFuture, ButtplugProtocol, ButtplugProtocolCommandHandler};
+use crate::{
+  core::messages::{self, ButtplugDeviceCommandMessageUnion, DeviceMessageAttributesMap},
+  device::{
+    protocol::{generic_command_manager::GenericCommandManager, ButtplugProtocolProperties},
+    DeviceImpl, DeviceWriteCmd, Endpoint,
+  },
+};
+use std::sync::Arc;
+use tokio::sync::Mutex;
+
+#[derive(ButtplugProtocolProperties)]
+pub struct LoveNuts {
+  name: String,
+  message_attributes: DeviceMessageAttributesMap,
+  manager: Arc<Mutex<GenericCommandManager>>,
+  stop_commands: Vec<ButtplugDeviceCommandMessageUnion>,
+}
+
+impl ButtplugProtocol for LoveNuts {
+  fn new_protocol(
+    name: &str,
+    message_attributes: DeviceMessageAttributesMap,
+  ) -> Box<dyn ButtplugProtocol> {
+    let manager = GenericCommandManager::new(&message_attributes);
+
+    Box::new(Self {
+      name: name.to_owned(),
+      message_attributes,
+      stop_commands: manager.get_stop_commands(),
+      manager: Arc::new(Mutex::new(manager)),
+    })
+  }
+}
+
+impl ButtplugProtocolCommandHandler for LoveNuts {
+  fn handle_vibrate_cmd(
+    &self,
+    device: Arc<DeviceImpl>,
+    message: messages::VibrateCmd,
+  ) -> ButtplugDeviceResultFuture {
+    let manager = self.manager.clone();
+    Box::pin(async move {
+      let result = manager.lock().await.update_vibration(&message, false)?;
+      if let Some(cmds) = result {
+        if let Some(speed) = cmds[0] {
+          let mut data: Vec<u8> = vec![ 0x45, 0x56, 0x4f, 0x4c ];
+          for _ in 0..10 {
+            let mut b: u8 = speed as u8;
+            b |= (speed as u8) << 4;
+            data.push(b);
+          }
+          data.push(0x00);
+          data.push(0xff);
+
+          device.write_value(DeviceWriteCmd::new(
+            Endpoint::Tx,
+            data,
+            false,
+          )).await?;
+        }
+      }
+
+      Ok(messages::Ok::default().into())
+    })
+  }
+}
+
+#[cfg(all(test, feature = "server"))]
+mod test {
+  use crate::{
+    core::messages::{StopDeviceCmd, VibrateCmd, VibrateSubcommand},
+    device::{DeviceImplCommand, DeviceWriteCmd, Endpoint},
+    test::{check_test_recv_value, new_bluetoothle_test_device},
+    util::async_manager,
+  };
+
+  #[test]
+  pub fn test_love_nuts_protocol() {
+    async_manager::block_on(async move {
+      let (device, test_device) = new_bluetoothle_test_device("Love_Nuts").await.unwrap();
+      device
+        .parse_message(VibrateCmd::new(0, vec![VibrateSubcommand::new(0, 0.5)]).into())
+        .await
+        .unwrap();
+      let command_receiver = test_device.get_endpoint_receiver(&Endpoint::Tx).unwrap();
+      check_test_recv_value(
+        &command_receiver,
+        DeviceImplCommand::Write(DeviceWriteCmd::new(
+          Endpoint::Tx,
+          vec![
+            0x45,
+            0x56,
+            0x4f,
+            0x4c,
+            0x88,
+            0x88,
+            0x88,
+            0x88,
+            0x88,
+            0x88,
+            0x88,
+            0x88,
+            0x88,
+            0x88,
+            0x00,
+            0xff,
+          ],
+          false,
+        )),
+      );
+      // Test to make sure we handle packet IDs across protocol clones correctly.
+      device
+        .parse_message(StopDeviceCmd::new(0).into())
+        .await
+        .unwrap();
+      check_test_recv_value(
+        &command_receiver,
+        DeviceImplCommand::Write(DeviceWriteCmd::new(
+          Endpoint::Tx,
+          vec![
+            0x45,
+            0x56,
+            0x4f,
+            0x4c,
+            0x00,
+            0x00,
+            0x00,
+            0x00,
+            0x00,
+            0x00,
+            0x00,
+            0x00,
+            0x00,
+            0x00,
+            0x00,
+            0xff,
+          ],
+          false,
+        )),
+      );
+    });
+  }
+}
