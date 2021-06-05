d9533ec0b2c193f7bd2f47f3be644d11fe271fbb
Kyle Machulis <kyle@nonpolynomial.com>
2021-06-05 14:00:41 -0700

    chore: Remove unused module

--- buttplug/src/server/comm_managers/btleplug/mod.rs
+++ buttplug/src/server/comm_managers/btleplug/mod.rs
@@ -2,4 +2,3 @@ pub mod btleplug_comm_manager;
 pub use btleplug_comm_manager::BtlePlugCommunicationManagerBuilder;
 pub mod btleplug_device_impl;
 mod btleplug_adapter_task;
-mod btleplug_device_task;
