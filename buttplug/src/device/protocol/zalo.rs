8f2ddcbe1db600b62b26687ab69f2b60beae43a3
blackspherefollower <darktech@black-sphere.co.uk>
2021-07-05 23:37:12 -0700

    feat: Adding support for Zalo devices
    Tested with the Jeanne, Queen and King
    
    The King's protocol was originally analysed by @TheHardew
    
    Other Zalo devices will require config updates as they appear.

---
+++ buttplug/src/device/protocol/zalo.rs
@@ -0,0 +1,168 @@
+use super::{ButtplugDeviceResultFuture, ButtplugProtocol, ButtplugProtocolCommandHandler};
+use crate::{
+  core::messages::{self, ButtplugDeviceCommandMessageUnion, DeviceMessageAttributesMap},
+  device::{
+    protocol::{generic_command_manager::GenericCommandManager, ButtplugProtocolProperties},
+    DeviceImpl, DeviceWriteCmd, Endpoint,
+  },
+};
+use std::sync::Arc;
+use tokio::sync::Mutex;
+
+#[derive(ButtplugProtocolProperties)]
+pub struct Zalo {
+  name: String,
+  message_attributes: DeviceMessageAttributesMap,
+  manager: Arc<Mutex<GenericCommandManager>>,
+  stop_commands: Vec<ButtplugDeviceCommandMessageUnion>,
+}
+
+impl ButtplugProtocol for Zalo {
+  fn new_protocol(
+    name: &str,
+    message_attributes: DeviceMessageAttributesMap,
+  ) -> Box<dyn ButtplugProtocol> {
+    let manager = GenericCommandManager::new(&message_attributes);
+
+    Box::new(Self {
+      name: name.to_owned(),
+      message_attributes,
+      stop_commands: manager.get_stop_commands(),
+      manager: Arc::new(Mutex::new(manager)),
+    })
+  }
+}
+
+impl ButtplugProtocolCommandHandler for Zalo {
+  fn handle_vibrate_cmd(
+    &self,
+    device: Arc<DeviceImpl>,
+    message: messages::VibrateCmd,
+  ) -> ButtplugDeviceResultFuture {
+    // Store off result before the match, so we drop the lock ASAP.
+    let manager = self.manager.clone();
+    Box::pin(async move {
+      let result = manager.lock().await.update_vibration(&message, true)?;
+      if let Some(cmds) = result {
+        let speed0: u8 = cmds[0].unwrap_or(0) as u8;
+        let speed1: u8 = if cmds.len() == 1 { 0 } else { cmds[1].unwrap_or(0) as u8 };
+        device.write_value(DeviceWriteCmd::new(
+          Endpoint::Tx,
+          vec![
+            if speed0 == 0 && speed1 == 0 { 0x02 } else { 0x01 },
+            if speed0 == 0 { 0x01 } else { speed0 },
+            if speed1 == 0 { 0x01 } else { speed1 },
+          ],
+          true,
+        )).await?;
+      }
+      Ok(messages::Ok::default().into())
+    })
+  }
+}
+
+#[cfg(all(test, feature = "server"))]
+mod test {
+  use crate::{
+    core::messages::{StopDeviceCmd, VibrateCmd, VibrateSubcommand},
+    device::{DeviceImplCommand, DeviceWriteCmd, Endpoint},
+    test::{check_test_recv_empty, check_test_recv_value, new_bluetoothle_test_device},
+    util::async_manager,
+  };
+
+  #[test]
+  pub fn test_zalo_protocol_1vibe() {
+    async_manager::block_on(async move {
+      let (device, test_device) = new_bluetoothle_test_device("ZALO-Jeanne").await.unwrap();
+      let command_receiver_tx = test_device.get_endpoint_receiver(&Endpoint::Tx).unwrap();
+      device
+        .parse_message(VibrateCmd::new(0, vec![VibrateSubcommand::new(0, 0.5)]).into())
+        .await
+        .unwrap();
+      check_test_recv_value(
+        &command_receiver_tx,
+        DeviceImplCommand::Write(DeviceWriteCmd::new(Endpoint::Tx, vec![0x01, 0x04, 0x01], true)),
+      );
+      assert!(check_test_recv_empty(&command_receiver_tx));
+
+      // Since we only created one subcommand, we should only receive one command.
+      device
+        .parse_message(VibrateCmd::new(0, vec![VibrateSubcommand::new(0, 0.5)]).into())
+        .await
+        .unwrap();
+      assert!(check_test_recv_empty(&command_receiver_tx));
+
+      device
+          .parse_message(VibrateCmd::new(0, vec![VibrateSubcommand::new(0, 1.0)]).into())
+          .await
+          .unwrap();
+      check_test_recv_value(
+        &command_receiver_tx,
+        DeviceImplCommand::Write(DeviceWriteCmd::new(Endpoint::Tx, vec![0x01, 0x08, 0x01], true)),
+      );
+      assert!(check_test_recv_empty(&command_receiver_tx));
+
+      device
+        .parse_message(StopDeviceCmd::new(0).into())
+        .await
+        .unwrap();
+      check_test_recv_value(
+        &command_receiver_tx,
+        DeviceImplCommand::Write(DeviceWriteCmd::new(Endpoint::Tx, vec![0x02, 0x01, 0x01], true)),
+      );
+      assert!(check_test_recv_empty(&command_receiver_tx));
+    });
+  }
+
+  #[test]
+  pub fn test_zalo_protocol_2vibe() {
+    async_manager::block_on(async move {
+      let (device, test_device) = new_bluetoothle_test_device("ZALO-Queen").await.unwrap();
+      let command_receiver_tx = test_device.get_endpoint_receiver(&Endpoint::Tx).unwrap();
+      device
+        .parse_message(
+          VibrateCmd::new(
+            0,
+            vec![
+              VibrateSubcommand::new(0, 0.5),
+              VibrateSubcommand::new(1, 0.5),
+            ],
+          )
+          .into(),
+        )
+        .await
+        .unwrap();
+      check_test_recv_value(
+        &command_receiver_tx,
+        DeviceImplCommand::Write(DeviceWriteCmd::new(Endpoint::Tx, vec![0x01, 0x04, 0x04], true)),
+      );
+      assert!(check_test_recv_empty(&command_receiver_tx));
+
+      // Since we only created one subcommand, we should only receive one command.
+      device
+        .parse_message(VibrateCmd::new(0, vec![VibrateSubcommand::new(1, 1.0)]).into())
+        .await
+        .unwrap();
+      check_test_recv_value(
+        &command_receiver_tx,
+        DeviceImplCommand::Write(DeviceWriteCmd::new(Endpoint::Tx, vec![0x01, 0x04, 0x08], true)),
+      );
+
+      device
+        .parse_message(VibrateCmd::new(0, vec![VibrateSubcommand::new(0, 0.5)]).into())
+        .await
+        .unwrap();
+      assert!(check_test_recv_empty(&command_receiver_tx));
+
+      device
+        .parse_message(StopDeviceCmd::new(0).into())
+        .await
+        .unwrap();
+      check_test_recv_value(
+        &command_receiver_tx,
+        DeviceImplCommand::Write(DeviceWriteCmd::new(Endpoint::Tx, vec![0x02, 0x01, 0x01], true)),
+      );
+      assert!(check_test_recv_empty(&command_receiver_tx));
+    });
+  }
+}
