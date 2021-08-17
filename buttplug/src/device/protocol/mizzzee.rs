2f7cff5a35730f3d406a4805be1b01c5efe795d4
blackspherefollower <darktech@black-sphere.co.uk>
2021-08-17 03:23:16 -0700

    feat: Adding support for the Mizz Zee Pill

---
+++ buttplug/src/device/protocol/mizzzee.rs
@@ -0,0 +1,110 @@
+use super::{ButtplugDeviceResultFuture, ButtplugProtocol, ButtplugProtocolCommandHandler};
+use crate::{
+  core::messages::{self, ButtplugDeviceCommandMessageUnion, DeviceMessageAttributesMap},
+  device::{
+    protocol::{generic_command_manager::GenericCommandManager, ButtplugProtocolProperties},
+    DeviceImpl, DeviceWriteCmd, Endpoint,
+  },
+};
+use std::sync::Arc;
+use tokio::sync::Mutex;
+
+#[derive(ButtplugProtocolProperties)]
+pub struct MizzZee {
+  name: String,
+  message_attributes: DeviceMessageAttributesMap,
+  manager: Arc<Mutex<GenericCommandManager>>,
+  stop_commands: Vec<ButtplugDeviceCommandMessageUnion>,
+}
+
+impl ButtplugProtocol for MizzZee {
+  fn new_protocol(
+    name: &str,
+    message_attributes: DeviceMessageAttributesMap,
+  ) -> Box<dyn ButtplugProtocol> {
+    let manager = GenericCommandManager::new(&message_attributes);
+
+    Box::new(Self {
+      name: name.to_owned(),
+      message_attributes,
+      stop_commands: manager.get_stop_commands(),
+      manager: Arc::new(Mutex::new(manager)),
+    })
+  }
+}
+
+impl ButtplugProtocolCommandHandler for MizzZee {
+  fn handle_vibrate_cmd(
+    &self,
+    device: Arc<DeviceImpl>,
+    message: messages::VibrateCmd,
+  ) -> ButtplugDeviceResultFuture {
+    let manager = self.manager.clone();
+    Box::pin(async move {
+      let result = manager.lock().await.update_vibration(&message, false)?;
+      if let Some(cmds) = result {
+        if let Some(speed) = cmds[0] {
+          device
+            .write_value(DeviceWriteCmd::new(
+              Endpoint::Tx,
+              vec![
+                0x69,
+                0x96,
+                0x03,
+                0x01,
+                if speed == 0 { 0x00 } else { 0x01 },
+                speed as u8,
+              ],
+              false,
+            ))
+            .await?;
+        }
+      }
+
+      Ok(messages::Ok::default().into())
+    })
+  }
+}
+
+#[cfg(all(test, feature = "server"))]
+mod test {
+  use crate::{
+    core::messages::{StopDeviceCmd, VibrateCmd, VibrateSubcommand},
+    device::{DeviceImplCommand, DeviceWriteCmd, Endpoint},
+    server::comm_managers::test::{check_test_recv_value, new_bluetoothle_test_device},
+    util::async_manager,
+  };
+
+  #[test]
+  pub fn test_mizz_zee_protocol() {
+    async_manager::block_on(async move {
+      let (device, test_device) = new_bluetoothle_test_device("NFY008").await.unwrap();
+      device
+        .parse_message(VibrateCmd::new(0, vec![VibrateSubcommand::new(0, 0.5)]).into())
+        .await
+        .unwrap();
+      let command_receiver = test_device.get_endpoint_receiver(&Endpoint::Tx).unwrap();
+      check_test_recv_value(
+        &command_receiver,
+        DeviceImplCommand::Write(DeviceWriteCmd::new(
+          Endpoint::Tx,
+          vec![0x69, 0x96, 0x03, 0x01, 0x01, 34],
+          false,
+        )),
+      );
+      // Test to make sure we handle packet IDs across protocol clones correctly.
+      device
+        .parse_message(StopDeviceCmd::new(0).into())
+        .await
+        .unwrap();
+      check_test_recv_value(
+        &command_receiver,
+        DeviceImplCommand::Write(DeviceWriteCmd::new(
+          Endpoint::Tx,
+          vec![0x69, 0x96, 0x03, 0x01, 0x00, 0x00],
+          false,
+        )),
+      );
+    });
+  }
+}
