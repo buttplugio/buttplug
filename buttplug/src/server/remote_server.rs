44b1f8335a9f768562c069d37e36f5ea2b9f2045
Kyle Machulis <kyle@nonpolynomial.com>
2021-10-23 14:44:42 -0700

    chore: Only emit server message updates when there are listeners
    Otherwise we'll get lots of errors about remote event sender not
    having any subscribers

--- buttplug/src/server/remote_server.rs
+++ buttplug/src/server/remote_server.rs
@@ -75,8 +75,10 @@ async fn run_server<ConnectorType>(
             match server_clone.parse_message(client_message.clone()).await {
               Ok(ret_msg) => {
                 if let ButtplugClientMessage::RequestServerInfo(rsi) = client_message {
-                  if remote_event_sender_clone.send(ButtplugRemoteServerEvent::Connected(rsi.client_name().clone())).is_err() {
-                    error!("Cannot send event to owner, dropping and assuming local server thread has exited.");
+                  if remote_event_sender_clone.receiver_count() > 0 {
+                    if remote_event_sender_clone.send(ButtplugRemoteServerEvent::Connected(rsi.client_name().clone())).is_err() {
+                      error!("Cannot send event to owner, dropping and assuming local server thread has exited.");
+                    }
                   }
                 }
                 if connector_clone.send(ret_msg).await.is_err() {
@@ -102,18 +104,20 @@ async fn run_server<ConnectorType>(
           break;
         }
         Some(msg) => {
-          match &msg {
-            ButtplugServerMessage::DeviceAdded(da) => {
-              if remote_event_sender.send(ButtplugRemoteServerEvent::DeviceAdded(da.device_index(), da.device_name().clone())).is_err() {
-                error!("Cannot send event to owner, dropping and assuming local server thread has exited.");
-              }
-            },
-            ButtplugServerMessage::DeviceRemoved(dr) => {
-             if remote_event_sender.send(ButtplugRemoteServerEvent::DeviceRemoved(dr.device_index())).is_err() {
-               error!("Cannot send event to owner, dropping and assuming local server thread has exited.");
-             }
-            },
-            _ => {}
+          if remote_event_sender.receiver_count() > 0 {
+            match &msg {
+              ButtplugServerMessage::DeviceAdded(da) => {
+                if remote_event_sender.send(ButtplugRemoteServerEvent::DeviceAdded(da.device_index(), da.device_name().clone())).is_err() {
+                  error!("Cannot send event to owner, dropping and assuming local server thread has exited.");
+                }
+              },
+              ButtplugServerMessage::DeviceRemoved(dr) => {
+               if remote_event_sender.send(ButtplugRemoteServerEvent::DeviceRemoved(dr.device_index())).is_err() {
+                 error!("Cannot send event to owner, dropping and assuming local server thread has exited.");
+               }
+              },
+              _ => {}
+            }  
           }
           let connector_clone = shared_connector.clone();
           if connector_clone.send(msg).await.is_err() {
