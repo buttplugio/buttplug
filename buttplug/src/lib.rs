bc177f0f2d8fd55c433b0ef9d555c20a74ff39a1
Kyle Machulis <kyle@nonpolynomial.com>
2021-12-03 14:58:23 -0800

    doc: Fix README inclusion so it doesn't break tests

--- buttplug/src/lib.rs
+++ buttplug/src/lib.rs
@@ -9,7 +9,7 @@
 #![crate_name = "buttplug"]
 // Required for select! expansion in RemoteServer
 #![recursion_limit = "512"]
-#[doc = include_str!("../README.md")]
+#![doc = include_str!("../README.md")]
 
 #[macro_use]
 extern crate buttplug_derive;
