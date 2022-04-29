3f3e1fe7d85022bda41fc8bf2efdb576bc754ca2
blackspherefollower <darktech@black-sphere.co.uk>
2022-04-29 00:29:43 -0700

    feat: Implement Hismith device type detection
    Adds support for the vibe on the thrusing cup

--- buttplug/src/device/protocol/hismith.rs
+++ buttplug/src/device/protocol/hismith.rs
@@ -4,13 +4,64 @@ use crate::{
   device::{
     protocol::{generic_command_manager::GenericCommandManager, ButtplugProtocolProperties},
     DeviceImpl,
+    DeviceReadCmd,
     DeviceWriteCmd,
     Endpoint,
   },
 };
-use std::sync::Arc;
+use std::sync::{Arc};
+use tokio::sync::Mutex;
 
-super::default_protocol_declaration!(Hismith);
+#[derive(ButtplugProtocolProperties)]
+pub struct Hismith {
+  name: String,
+  message_attributes: DeviceMessageAttributesMap,
+  manager: Arc<Mutex<GenericCommandManager>>,
+  stop_commands: Vec<ButtplugDeviceCommandMessageUnion>,
+}
+
+impl Hismith {
+  fn new(
+    name: &str,
+    message_attributes: DeviceMessageAttributesMap,
+  ) -> Self {
+    let manager = GenericCommandManager::new(&message_attributes);
+    Self {
+      name: name.to_owned(),
+      message_attributes,
+      stop_commands: manager.get_stop_commands(),
+      manager: Arc::new(Mutex::new(manager)),
+    }
+  }
+}
+
+impl ButtplugProtocol for Hismith {
+  fn try_create(
+    device_impl: Arc<crate::device::DeviceImpl>,
+    config: crate::device::protocol::DeviceProtocolConfiguration,
+  ) -> futures::future::BoxFuture<
+    'static,
+    Result<Box<dyn ButtplugProtocol>, crate::core::errors::ButtplugError>,
+  > {
+    Box::pin(async move {
+      let result = device_impl
+          .read_value(DeviceReadCmd::new(Endpoint::RxBLEModel, 128, 500))
+          .await?;
+      let device_identifier = result.data().into_iter().map( |b| format!("{:02x}", b) ).collect::<String>();
+      info!(
+        "Hismith Device Identifier: {}",
+        device_identifier
+      );
+      let (name, attrs) = crate::device::protocol::get_protocol_features(
+        device_impl.clone(),
+        Some(device_identifier),
+        config,
+      )?;
+      let device = Self::new(&name, attrs);
+      Ok(Box::new(device) as Box<dyn ButtplugProtocol>)
+    })
+  }
+}
 
 impl ButtplugProtocolCommandHandler for Hismith {
   fn handle_vibrate_cmd(
@@ -30,6 +81,16 @@ impl ButtplugProtocolCommandHandler for Hismith {
             vec![0xAA, 0x04, speed as u8, (speed + 4) as u8],
             false,
           )));
+        }
+        if cmds.len() > 1 {
+          if let Some(speed) = cmds[1] {
+            let value = if speed == 0 { 0xf0 } else { speed };
+            fut_vec.push(device.write_value(DeviceWriteCmd::new(
+              Endpoint::Tx,
+              vec![0xAA, 0x06, value as u8, (value + 6) as u8],
+              false,
+            )));
+          }
         }
       }
       // TODO Just use join_all here
