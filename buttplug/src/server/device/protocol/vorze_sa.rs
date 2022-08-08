d345dc28cad5af2648b3f2016b0a4dce351a4d06
blackspherefollower <darktech@black-sphere.co.uk>
2022-08-08 06:40:00 -0700

    feat: Adding support for Vorze UFO TW

--- buttplug/src/server/device/protocol/vorze_sa.rs
+++ buttplug/src/server/device/protocol/vorze_sa.rs
@@ -42,7 +42,7 @@ impl ProtocolInitializer for VorzeSAInitializer {
     let hwname = hardware.name().to_ascii_lowercase();
     let device_type = if hwname.contains("cyclone") {
       VorzeDevice::Cyclone
-    } else if hwname.contains("ufo tw") {
+    } else if hwname.contains("ufo-tw") {
       VorzeDevice::UfoTw
     } else if hwname.contains("ufo") {
       VorzeDevice::Ufo
@@ -142,16 +142,39 @@ impl ProtocolHandler for VorzeSA {
     &self,
     cmds: &Vec<Option<(u32, bool)>>,
   ) -> Result<Vec<HardwareCommand>, ButtplugDeviceError> {
-    if let Some((speed, clockwise)) = cmds[0] {
-      let data: u8 = (clockwise as u8) << 7 | (speed as u8);
-      Ok(vec![HardwareWriteCmd::new(
-        Endpoint::Tx,
-        vec![self.device_type as u8, VorzeActions::Rotate as u8, data],
-        true,
-      )
-      .into()])
+    if cmds.len() == 1 {
+      if let Some((speed, clockwise)) = cmds[0] {
+        let data: u8 = (clockwise as u8) << 7 | (speed as u8);
+        Ok(vec![HardwareWriteCmd::new(
+          Endpoint::Tx,
+          vec![self.device_type as u8, VorzeActions::Rotate as u8, data],
+          true,
+        )
+            .into()])
+      } else {
+        Ok(vec![])
+      }
     } else {
-      Ok(vec![])
+      let mut data_left = 0u8;
+      let mut data_right = 0u8;
+      let mut changed = false;
+      if let Some((speed, clockwise)) = cmds[0] {
+        data_left = (clockwise as u8) << 7 | (speed as u8);
+        changed = true;
+      }
+      if let Some((speed, clockwise)) = cmds[1] {
+        data_right = (clockwise as u8) << 7 | (speed as u8);
+        changed = true;
+      }
+      if changed {
+        Ok(vec![HardwareWriteCmd::new(
+          Endpoint::Tx,
+          vec![self.device_type as u8, data_left, data_right],
+          true)
+        .into()])
+      } else {
+        Ok(vec![])
+      }
     }
   }
 
@@ -186,216 +209,3 @@ impl ProtocolHandler for VorzeSA {
     self.handle_rotate_cmd(&vec![Some((msg.speed(), msg.clockwise()))])
   }
 }
-
-/*
-#[cfg(all(test, feature = "server"))]
-mod test {
-  use crate::{
-    core::messages::{
-      Endpoint,
-      LinearCmd,
-      RotateCmd,
-      RotationSubcommand,
-      StopDeviceCmd,
-      VectorSubcommand,
-      VibrateCmd,
-      VibrateSubcommand,
-    },
-    server::device::{
-      hardware::{HardwareCommand, HardwareWriteCmd},
-    hardware::communication::test::{
-      check_test_recv_empty,
-      check_test_recv_value,
-      new_bluetoothle_test_device,
-    },
-  },
-    util::async_manager,
-  };
-
-  #[test]
-  pub fn test_vorze_sa_vibration_protocol_bach() {
-    async_manager::block_on(async move {
-      let (device, test_device) = new_bluetoothle_test_device("Bach smart")
-        .await
-        .expect("Test, assuming infallible");
-      let command_receiver = test_device
-        .endpoint_receiver(&Endpoint::Tx)
-        .expect("Test, assuming infallible");
-      device
-        .parse_message(VibrateCmd::new(0, vec![VibrateSubcommand::new(0, 0.5)]).into())
-        .await
-        .expect("Test, assuming infallible");
-      check_test_recv_value(
-        &command_receiver,
-        HardwareCommand::Write(HardwareWriteCmd::new(
-          Endpoint::Tx,
-          vec![0x06, 0x03, 50],
-          true,
-        )),
-      );
-      assert!(check_test_recv_empty(&command_receiver));
-
-      device
-        .parse_message(StopDeviceCmd::new(0).into())
-        .await
-        .expect("Test, assuming infallible");
-      check_test_recv_value(
-        &command_receiver,
-        HardwareCommand::Write(HardwareWriteCmd::new(
-          Endpoint::Tx,
-          vec![0x06, 0x03, 0x0],
-          true,
-        )),
-      );
-      assert!(check_test_recv_empty(&command_receiver));
-    });
-  }
-
-  #[test]
-  pub fn test_vorze_sa_vibration_protocol_rocket() {
-    async_manager::block_on(async move {
-      let (device, test_device) = new_bluetoothle_test_device("ROCKET")
-        .await
-        .expect("Test, assuming infallible");
-      let command_receiver = test_device
-        .endpoint_receiver(&Endpoint::Tx)
-        .expect("Test, assuming infallible");
-      device
-        .parse_message(VibrateCmd::new(0, vec![VibrateSubcommand::new(0, 0.5)]).into())
-        .await
-        .expect("Test, assuming infallible");
-      check_test_recv_value(
-        &command_receiver,
-        HardwareCommand::Write(HardwareWriteCmd::new(
-          Endpoint::Tx,
-          vec![0x07, 0x03, 50],
-          true,
-        )),
-      );
-      assert!(check_test_recv_empty(&command_receiver));
-
-      device
-        .parse_message(StopDeviceCmd::new(0).into())
-        .await
-        .expect("Test, assuming infallible");
-      check_test_recv_value(
-        &command_receiver,
-        HardwareCommand::Write(HardwareWriteCmd::new(
-          Endpoint::Tx,
-          vec![0x07, 0x03, 0x0],
-          true,
-        )),
-      );
-      assert!(check_test_recv_empty(&command_receiver));
-    });
-  }
-
-  #[test]
-  pub fn test_vorze_sa_rotation_protocol() {
-    async_manager::block_on(async move {
-      let (device, test_device) = new_bluetoothle_test_device("CycSA")
-        .await
-        .expect("Test, assuming infallible");
-      let command_receiver = test_device
-        .endpoint_receiver(&Endpoint::Tx)
-        .expect("Test, assuming infallible");
-      device
-        .parse_message(RotateCmd::new(0, vec![RotationSubcommand::new(0, 0.5, false)]).into())
-        .await
-        .expect("Test, assuming infallible");
-      check_test_recv_value(
-        &command_receiver,
-        HardwareCommand::Write(HardwareWriteCmd::new(
-          Endpoint::Tx,
-          vec![0x01, 0x01, 50],
-          true,
-        )),
-      );
-      assert!(check_test_recv_empty(&command_receiver));
-
-      device
-        .parse_message(RotateCmd::new(0, vec![RotationSubcommand::new(0, 0.5, true)]).into())
-        .await
-        .expect("Test, assuming infallible");
-      check_test_recv_value(
-        &command_receiver,
-        HardwareCommand::Write(HardwareWriteCmd::new(
-          Endpoint::Tx,
-          vec![0x01, 0x01, 178],
-          true,
-        )),
-      );
-      assert!(check_test_recv_empty(&command_receiver));
-
-      device
-        .parse_message(StopDeviceCmd::new(0).into())
-        .await
-        .expect("Test, assuming infallible");
-      check_test_recv_value(
-        &command_receiver,
-        HardwareCommand::Write(HardwareWriteCmd::new(
-          Endpoint::Tx,
-          vec![0x01, 0x01, 0x0],
-          true,
-        )),
-      );
-      assert!(check_test_recv_empty(&command_receiver));
-    });
-  }
-
-  #[test]
-  pub fn test_vorze_sa_linear_protocol() {
-    async_manager::block_on(async move {
-      let (device, test_device) = new_bluetoothle_test_device("VorzePiston")
-        .await
-        .expect("Test, assuming infallible");
-      let command_receiver = test_device
-        .endpoint_receiver(&Endpoint::Tx)
-        .expect("Test, assuming infallible");
-      device
-        .parse_message(LinearCmd::new(0, vec![VectorSubcommand::new(0, 150, 0.95)]).into())
-        .await
-        .expect("Test, assuming infallible");
-      check_test_recv_value(
-        &command_receiver,
-        HardwareCommand::Write(HardwareWriteCmd::new(Endpoint::Tx, vec![0x03, 190, 92], true)),
-      );
-      assert!(check_test_recv_empty(&command_receiver));
-
-      device
-        .parse_message(LinearCmd::new(0, vec![VectorSubcommand::new(0, 150, 0.95)]).into())
-        .await
-        .expect("Test, assuming infallible");
-      check_test_recv_value(
-        &command_receiver,
-        HardwareCommand::Write(HardwareWriteCmd::new(
-          Endpoint::Tx,
-          vec![0x03, 190, 100],
-          true,
-        )),
-      );
-      assert!(check_test_recv_empty(&command_receiver));
-
-      device
-        .parse_message(LinearCmd::new(0, vec![VectorSubcommand::new(0, 50, 0.5)]).into())
-        .await
-        .expect("Test, assuming infallible");
-      check_test_recv_value(
-        &command_receiver,
-        HardwareCommand::Write(HardwareWriteCmd::new(
-          Endpoint::Tx,
-          vec![0x03, 100, 100],
-          true,
-        )),
-      );
-      assert!(check_test_recv_empty(&command_receiver));
-
-      device
-        .parse_message(StopDeviceCmd::new(0).into())
-        .await
-        .expect("Test, assuming infallible");
-      assert!(check_test_recv_empty(&command_receiver));
-    });
-  }
-}
-*/
