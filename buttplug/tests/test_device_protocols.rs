c0e91d024f75a312d8f0784fe02eb659c361570b
blackspherefollower <darktech@black-sphere.co.uk>
2022-10-08 19:01:12 -0700

    fix: Correct an issue with multi-vibe MagicMotion devices
    Also portig the tests to the new system

--- buttplug/tests/test_device_protocols.rs
+++ buttplug/tests/test_device_protocols.rs
@@ -53,6 +53,12 @@ async fn load_test_case(test_file: &str) -> DeviceTestCase {
 #[test_case("test_synchro_protocol.yaml" ; "Synchro Protocol")]
 #[test_case("test_lelo_tianiharmony.yaml" ; "Lelo Harmony Protocol - Tiani Harmony")]
 #[test_case("test_lelo_idawave.yaml" ; "Lelo Harmony Protocol - Ida Wave")]
+#[test_case("test_magic_motion_1_magic_cell.yaml" ; "MagicMotion Protocol 1 - Magic Cell")]
+#[test_case("test_magic_motion_2_eidolon.yaml" ; "MagicMotion Protocol 2 - Eidolon")]
+#[test_case("test_magic_motion_2_equinox.yaml" ; "MagicMotion Protocol 2 - Equinox")]
+#[test_case("test_magic_motion_3_krush.yaml" ; "MagicMotion Protocol 3 - Krush")]
+#[test_case("test_magic_motion_4_bobi.yaml" ; "MagicMotion Protocol 4 - Bobi")]
+#[test_case("test_magic_motion_4_nyx.yaml" ; "MagicMotion Protocol 4 - Nyx")]
 fn test_device_protocols_embedded_v3(test_file: &str) {
   //tracing_subscriber::fmt::init();
   async_manager::block_on(async {
@@ -97,6 +103,12 @@ fn test_device_protocols_embedded_v3(test_file: &str) {
 #[test_case("test_synchro_protocol.yaml" ; "Synchro Protocol")]
 #[test_case("test_lelo_tianiharmony.yaml" ; "Lelo Harmony Protocol - Tiani Harmony")]
 #[test_case("test_lelo_idawave.yaml" ; "Lelo Harmony Protocol - Ida Wave")]
+#[test_case("test_magic_motion_1_magic_cell.yaml" ; "MagicMotion Protocol 1 - Magic Cell")]
+#[test_case("test_magic_motion_2_eidolon.yaml" ; "MagicMotion Protocol 2 - Eidolon")]
+#[test_case("test_magic_motion_2_equinox.yaml" ; "MagicMotion Protocol 2 - Equinox")]
+#[test_case("test_magic_motion_3_krush.yaml" ; "MagicMotion Protocol 3 - Krush")]
+#[test_case("test_magic_motion_4_bobi.yaml" ; "MagicMotion Protocol 4 - Bobi")]
+#[test_case("test_magic_motion_4_nyx.yaml" ; "MagicMotion Protocol 4 - Nyx")]
 fn test_device_protocols_json_v3(test_file: &str) {
   //tracing_subscriber::fmt::init();
   async_manager::block_on(async {
@@ -135,6 +147,12 @@ fn test_device_protocols_json_v3(test_file: &str) {
 #[test_case("test_sakuraneko_protocol.yaml" ; "Sakuraneko Protocol")]
 #[test_case("test_synchro_protocol.yaml" ; "Synchro Protocol")]
 #[test_case("test_lelo_tianiharmony.yaml" ; "Lelo Harmony Protocol - Tiani Harmony")]
+#[test_case("test_magic_motion_1_magic_cell.yaml" ; "MagicMotion Protocol 1 - Magic Cell")]
+#[test_case("test_magic_motion_2_eidolon.yaml" ; "MagicMotion Protocol 2 - Eidolon")]
+#[test_case("test_magic_motion_2_equinox.yaml" ; "MagicMotion Protocol 2 - Equinox")]
+#[test_case("test_magic_motion_3_krush.yaml" ; "MagicMotion Protocol 3 - Krush")]
+#[test_case("test_magic_motion_4_bobi.yaml" ; "MagicMotion Protocol 4 - Bobi")]
+#[test_case("test_magic_motion_4_nyx.yaml" ; "MagicMotion Protocol 4 - Nyx")]
 fn test_device_protocols_embedded_v2(test_file: &str) {
   async_manager::block_on(async {
     util::device_test::client::client_v2::run_embedded_test_case(&load_test_case(test_file).await)
@@ -172,6 +190,12 @@ fn test_device_protocols_embedded_v2(test_file: &str) {
 #[test_case("test_sakuraneko_protocol.yaml" ; "Sakuraneko Protocol")]
 #[test_case("test_synchro_protocol.yaml" ; "Synchro Protocol")]
 #[test_case("test_lelo_tianiharmony.yaml" ; "Lelo Harmony Protocol - Tiani Harmony")]
+#[test_case("test_magic_motion_1_magic_cell.yaml" ; "MagicMotion Protocol 1 - Magic Cell")]
+#[test_case("test_magic_motion_2_eidolon.yaml" ; "MagicMotion Protocol 2 - Eidolon")]
+#[test_case("test_magic_motion_2_equinox.yaml" ; "MagicMotion Protocol 2 - Equinox")]
+#[test_case("test_magic_motion_3_krush.yaml" ; "MagicMotion Protocol 3 - Krush")]
+#[test_case("test_magic_motion_4_bobi.yaml" ; "MagicMotion Protocol 4 - Bobi")]
+#[test_case("test_magic_motion_4_nyx.yaml" ; "MagicMotion Protocol 4 - Nyx")]
 fn test_device_protocols_json_v2(test_file: &str) {
   async_manager::block_on(async {
     util::device_test::client::client_v2::run_json_test_case(&load_test_case(test_file).await)
