8155bc9621621bdf5f79a9e2e423bd0052e5deee
Kyle Machulis <kyle@nonpolynomial.com>
2022-10-15 12:44:54 -0700

    test: Update tests for v2 config change

--- buttplug/tests/test_device_config.rs
+++ buttplug/tests/test_device_config.rs
@@ -59,7 +59,7 @@ const BASE_CONFIG_JSON: &str = r#"
 const BASE_VALID_VERSION_CONFIG_JSON: &str = r#"
 {
   "version": {
-    "major": 1,
+    "major": 2,
     "minor": 999
   }
 }
@@ -77,7 +77,7 @@ const BASE_INVALID_VERSION_CONFIG_JSON: &str = r#"
 const BASE_VALID_NULL_USER_CONFIG_JSON: &str = r#"
 {
   "version": {
-    "major": 1,
+    "major": 2,
     "minor": 999
   },
   "user-configs": {}
