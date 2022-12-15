05b3afd08ecc076d818b24a50cebbf4a60d516bc
blackspherefollower <darktech@black-sphere.co.uk>
2022-12-15 12:59:48 +0000

    fix: Correct the Hgod protocol's command loop
    Previously Hgod devices would start but never stop

--- buttplug/src/server/device/protocol/hgod.rs
+++ buttplug/src/server/device/protocol/hgod.rs
@@ -5,6 +5,7 @@
 // Licensed under the BSD 3-Clause license. See LICENSE file in the project root
 // for full license information.
 
+use crate::core::message::ActuatorType;
 use crate::server::device::configuration::ProtocolDeviceAttributes;
 use crate::{
   core::{errors::ButtplugDeviceError, message::Endpoint},
@@ -22,15 +23,13 @@ use crate::{
   util::async_manager,
 };
 use async_trait::async_trait;
-use futures::FutureExt;
 use std::{
   sync::{
-    atomic::{AtomicBool, Ordering},
+    atomic::{AtomicU8, Ordering},
     Arc,
   },
   time::Duration,
 };
-use tokio::{sync::RwLock, time::sleep};
 
 // Time between Hgod update commands, in milliseconds.
 const HGOD_COMMAND_DELAY_MS: u64 = 100;
@@ -47,70 +46,56 @@ impl ProtocolInitializer for HgodInitializer {
     hardware: Arc<Hardware>,
     _: &ProtocolDeviceAttributes,
   ) -> Result<Arc<dyn ProtocolHandler>, ButtplugDeviceError> {
-    Ok(Arc::new(Hgod::new(&hardware)))
+    Ok(Arc::new(Hgod::new(hardware)))
   }
 }
 
 pub struct Hgod {
-  hardware: Arc<Hardware>,
-  current_command: Arc<RwLock<Vec<u8>>>,
-  updater_running: Arc<AtomicBool>,
+  last_command: Arc<AtomicU8>,
 }
 
 impl Hgod {
-  fn new(hardware: &Arc<Hardware>) -> Self {
-    Self {
-      hardware: hardware.clone(),
-      updater_running: Arc::new(AtomicBool::new(false)),
-      current_command: Arc::new(RwLock::new(vec![0x55, 0x04, 0, 0, 0, 0])),
-    }
+  fn new(hardware: Arc<Hardware>) -> Self {
+    let last_command = Arc::new(AtomicU8::new(0));
+
+    let last_command_clone = last_command.clone();
+    async_manager::spawn(async move {
+      send_hgod_updates(hardware, last_command_clone).await;
+    });
+
+    Self { last_command }
   }
 }
 
-async fn vibration_update_handler(device: Arc<Hardware>, command_holder: Arc<RwLock<Vec<u8>>>) {
-  info!("Entering Hgod Control Loop");
-  let mut current_command = command_holder.read().await.clone();
-  while device
-    .write_value(&HardwareWriteCmd::new(Endpoint::Tx, current_command, true))
-    .await
-    .is_ok()
-  {
-    sleep(Duration::from_millis(HGOD_COMMAND_DELAY_MS)).await;
-    current_command = command_holder.read().await.clone();
-    info!("Hgod Command: {:?}", current_command);
+// HGod toys vibes only last ~100ms seconds.
+async fn send_hgod_updates(device: Arc<Hardware>, data: Arc<AtomicU8>) {
+  loop {
+    let speed = data.load(Ordering::SeqCst);
+    let command = vec![0x55, 0x04, 0, 0, 0, speed];
+    if speed > 0 {
+      if let Err(e) = device
+        .write_value(&HardwareWriteCmd::new(Endpoint::Tx, command, false))
+        .await
+      {
+        error!(
+          "Got an error from a hgod device, exiting control loop: {:?}",
+          e
+        );
+        break;
+      }
+    }
+    tokio::time::sleep(Duration::from_millis(HGOD_COMMAND_DELAY_MS)).await;
   }
-  info!("Hgod control loop exiting, most likely due to device disconnection.");
 }
 
 impl ProtocolHandler for Hgod {
-  fn handle_scalar_vibrate_cmd(
+  fn handle_scalar_cmd(
     &self,
-    _index: u32,
-    scalar: u32,
+    commands: &[Option<(ActuatorType, u32)>],
   ) -> Result<Vec<HardwareCommand>, ButtplugDeviceError> {
-    let current_command = self.current_command.clone();
-    let update_running = self.updater_running.clone();
-    let hardware = self.hardware.clone();
-    async_manager::spawn(
-      async move {
-        let write_mutex = current_command.clone();
-        let mut command_writer = write_mutex.write().await;
-        let command: Vec<u8> = vec![0x55, 0x04, 0, 0, 0, scalar as u8];
-        *command_writer = command;
-        if !update_running.load(Ordering::SeqCst) {
-          async_manager::spawn(
-            async move { vibration_update_handler(hardware, current_command).await },
-          );
-          update_running.store(true, Ordering::SeqCst);
-        }
-      }
-      .boxed(),
-    );
+    if let Some(cmd) = commands[0] {
+      self.last_command.store(cmd.1 as u8, Ordering::SeqCst);
+    }
     Ok(vec![])
   }
 }
-
-// TODO Write some tests!
-//
-// At least, once I figure out how to do that with the weird timing on this
-// thing.
