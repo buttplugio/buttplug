285139b2897dc45e674c2c40edc33241c60f4380
blackspherefollower <darktech@black-sphere.co.uk>
2023-03-28 19:34:59 +0100

    feat: Adding support for the Auxfun Remote Control Box
    This is a little device by the Hismith brand Auxfun to add remote
    and app control to their low spec fucking machines.

---
+++ buttplug/src/server/device/protocol/hismith_mini.rs
@@ -0,0 +1,125 @@
+// Buttplug Rust Source Code File - See https://buttplug.io for more info.
+//
+// Copyright 2016-2023 Nonpolynomial Labs LLC. All rights reserved.
+//
+// Licensed under the BSD 3-Clause license. See LICENSE file in the project root
+// for full license information.
+
+use crate::server::device::configuration::ProtocolDeviceAttributes;
+use crate::{
+  core::{errors::ButtplugDeviceError, message::Endpoint},
+  server::device::{
+    configuration::ProtocolAttributesType,
+    hardware::{Hardware, HardwareCommand, HardwareReadCmd, HardwareWriteCmd},
+    protocol::{ProtocolHandler, ProtocolIdentifier, ProtocolInitializer},
+    ServerDeviceIdentifier,
+  },
+};
+use async_trait::async_trait;
+use std::sync::Arc;
+
+pub mod setup {
+  use crate::server::device::protocol::{ProtocolIdentifier, ProtocolIdentifierFactory};
+  #[derive(Default)]
+  pub struct HismithMiniIdentifierFactory {}
+
+  impl ProtocolIdentifierFactory for HismithMiniIdentifierFactory {
+    fn identifier(&self) -> &str {
+      "hismith-mini"
+    }
+
+    fn create(&self) -> Box<dyn ProtocolIdentifier> {
+      Box::new(super::HismithMiniIdentifier::default())
+    }
+  }
+}
+
+#[derive(Default)]
+pub struct HismithMiniIdentifier {}
+
+#[async_trait]
+impl ProtocolIdentifier for HismithMiniIdentifier {
+  async fn identify(
+    &mut self,
+    hardware: Arc<Hardware>,
+  ) -> Result<(ServerDeviceIdentifier, Box<dyn ProtocolInitializer>), ButtplugDeviceError> {
+    let result = hardware
+      .read_value(&HardwareReadCmd::new(Endpoint::RxBLEModel, 128, 500))
+      .await?;
+
+    let identifier = result
+      .data()
+      .iter()
+      .map(|b| format!("{:02x}", b))
+      .collect::<String>();
+    info!("Hismith Device Identifier: {}", identifier);
+
+    Ok((
+      ServerDeviceIdentifier::new(
+        hardware.address(),
+        "hismith-mini",
+        &ProtocolAttributesType::Identifier(identifier),
+      ),
+      Box::new(HismithMiniInitializer::default()),
+    ))
+  }
+}
+
+#[derive(Default)]
+pub struct HismithMiniInitializer {}
+
+#[async_trait]
+impl ProtocolInitializer for HismithMiniInitializer {
+  async fn initialize(
+    &mut self,
+    _: Arc<Hardware>,
+    _: &ProtocolDeviceAttributes,
+  ) -> Result<Arc<dyn ProtocolHandler>, ButtplugDeviceError> {
+    Ok(Arc::new(HismithMini::default()))
+  }
+}
+
+#[derive(Default)]
+pub struct HismithMini {}
+
+impl ProtocolHandler for HismithMini {
+  fn handle_scalar_oscillate_cmd(
+    &self,
+    _index: u32,
+    scalar: u32,
+  ) -> Result<Vec<HardwareCommand>, ButtplugDeviceError> {
+    let idx: u8 = if _index == 0 { 0x03 } else { 0x06 };
+    let speed: u8 = if _index != 0 && scalar == 0 {
+      0xf0
+    } else {
+      scalar as u8
+    };
+
+    Ok(vec![HardwareWriteCmd::new(
+      Endpoint::Tx,
+      vec![0xCC, idx, speed, speed + idx],
+      false,
+    )
+    .into()])
+  }
+
+  fn handle_scalar_vibrate_cmd(
+    &self,
+    _index: u32,
+    scalar: u32,
+  ) -> Result<Vec<HardwareCommand>, ButtplugDeviceError> {
+    let idx: u8 = if _index == 0 { 0x03 } else { 0x06 };
+    let speed: u8 = if _index != 0 && scalar == 0 {
+      0xf0
+    } else {
+      scalar as u8
+    };
+
+    Ok(vec![HardwareWriteCmd::new(
+      Endpoint::Tx,
+      vec![0xCC, idx, speed, speed + idx],
+      false,
+    )
+    .into()])
+  }
+}
