dc4afab3c8010a449e3e2ae53c6562dd953b658c
Kyle Machulis <kyle@nonpolynomial.com>
2023-04-09 23:11:15 -0700

    feat: Add new diesel files
    Add beginnings of schema/migration files for sqlite.

---
+++ buttplug/migrations/2023-04-08-201045_base_db_creation/down.sql
@@ -0,0 +1,23 @@
+-- This file should undo anything in `up.sql`
+
+DROP TABLE protocol_bluetooth_name;
+DROP TABLE protocol_bluetooth_manufacturer_data;
+DROP TABLE protocol_bluetooth_prefix;
+DROP TABLE protocol_bluetooth_characteristic;
+DROP TABLE protocol_bluetooth_service;
+DROP TABLE protocol_xinput;
+DROP TABLE protocol_serial;
+DROP TABLE protocol_hid;
+DROP TABLE protocol_usb;
+DROP TABLE user_protocol_serial;
+DROP TABLE user_protocol_websocket_name;
+DROP TABLE user_protocol_websocket_prefix;
+DROP TABLE feature_linearcmd;
+DROP TABLE feature_rotatecmd;
+DROP TABLE feature_scalarcmd;
+DROP TABLE feature_sensorcmd;
+DROP TABLE device_feature;
+DROP TABLE device;
+DROP TABLE protocol;
+DROP TABLE actuator_type;
+DROP TABLE sensor_type;
