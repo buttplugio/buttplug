ec299dedfc3026d76bb981583e55550676b00227
blackspherefollower <darktech@black-sphere.co.uk>
2023-09-23 18:17:33 +0100

    feat: Adding support for Svakom Erica and ToyCod Vanesia Correcting individual motor control on Barzillai

--- buttplug/src/server/device/protocol/svakom_v4.rs
+++ buttplug/src/server/device/protocol/svakom_v4.rs
@@ -5,6 +5,7 @@
 // Licensed under the BSD 3-Clause license. See LICENSE file in the project root
 // for full license information.
 
+use crate::core::message::ActuatorType;
 use crate::{
   core::{errors::ButtplugDeviceError, message::Endpoint},
   server::device::{
@@ -19,17 +20,33 @@ generic_protocol_setup!(SvakomV4, "svakom-v4");
 pub struct SvakomV4 {}
 
 impl ProtocolHandler for SvakomV4 {
-  fn handle_scalar_vibrate_cmd(
+  fn needs_full_command_set(&self) -> bool {
+    true
+  }
+
+  fn handle_scalar_cmd(
     &self,
-    _index: u32,
-    scalar: u32,
+    commands: &[Option<(ActuatorType, u32)>],
   ) -> Result<Vec<HardwareCommand>, ButtplugDeviceError> {
+    let mut actuator: u8 = 0;
+    let mut scalar: u8 = 0;
+    for i in 0..commands.len() {
+      if let Some(cmd) = commands[i] {
+        if cmd.1 != 0 && scalar == 0 && commands.len() > 1 {
+          actuator = i as u8 + 1; // just this actuators
+        } else if cmd.1 != 0 {
+          actuator = 0; // all the actuators
+        }
+        scalar = u8::max(scalar, cmd.1 as u8); // max of all actuators
+      }
+    }
+
     Ok(vec![HardwareWriteCmd::new(
       Endpoint::Tx,
       [
         0x55,
         0x03,
-        0x00,
+        actuator,
         0x00,
         if scalar == 0 { 0x00 } else { 0x01 },
         scalar as u8,
