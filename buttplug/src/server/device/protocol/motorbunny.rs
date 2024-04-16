013d4d7e4450d30c7f4b0c809a21c3ef92d727c8
Kyle Machulis <kyle@nonpolynomial.com>
2024-04-15 22:07:14 -0700

    doc: Update copyright years on the files that have the header

--- buttplug/src/server/device/protocol/motorbunny.rs
+++ buttplug/src/server/device/protocol/motorbunny.rs
@@ -1,13 +1,13 @@
 // Buttplug Rust Source Code File - See https://buttplug.io for more info.
 //
-// Copyright 2016-2022 Nonpolynomial Labs LLC. All rights reserved.
+// Copyright 2016-2024 Nonpolynomial Labs LLC. All rights reserved.
 //
 // Licensed under the BSD 3-Clause license. See LICENSE file in the project root
 // for full license information.
 
 // Buttplug Rust Source Code File - See https://buttplug.io for more info.
 //
-// Copyright 2016-2022 Nonpolynomial Labs LLC. All rights reserved.
+// Copyright 2016-2024 Nonpolynomial Labs LLC. All rights reserved.
 //
 // Licensed under the BSD 3-Clause license. See LICENSE file in the project root
 // for full license information.
