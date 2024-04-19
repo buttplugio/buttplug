6a416c2ce99bcaf17b8d41782b23fd6f14f64c82
blackspherefollower <darktech@black-sphere.co.uk>
2024-04-19 10:41:12 +0100

    fix: Correct a timing issue with a RwLock in JoyHub devices

--- buttplug/src/server/device/protocol/joyhub_v2.rs
+++ buttplug/src/server/device/protocol/joyhub_v2.rs
@@ -50,7 +50,7 @@ fn vibes_changed(
   new_commands: &[Option<(ActuatorType, u32)>],
   exclude: Vec<usize>,
 ) -> bool {
-  let old_commands = old_commands_lock.try_read().expect("locks should work");
+  let old_commands = old_commands_lock.read().expect("locks should work");
   if old_commands.len() != new_commands.len() {
     return true;
   }
@@ -128,7 +128,7 @@ impl ProtocolHandler for JoyHubV2 {
           async_manager::spawn(async move { delayed_constrict_handler(dev, cmd.1 as u8).await });
           cmd2 = None;
         } else {
-          let mut command_writer = self.last_cmds.try_write().expect("Locks should work");
+          let mut command_writer = self.last_cmds.write().expect("Locks should work");
           *command_writer = commands.to_vec();
 
           return Ok(vec![HardwareWriteCmd::new(
@@ -148,7 +148,7 @@ impl ProtocolHandler for JoyHubV2 {
           async_manager::spawn(async move { delayed_constrict_handler(dev, cmd.1 as u8).await });
           cmd3 = None;
         } else {
-          let mut command_writer = self.last_cmds.try_write().expect("Locks should work");
+          let mut command_writer = self.last_cmds.write().expect("Locks should work");
           *command_writer = commands.to_vec();
 
           return Ok(vec![HardwareWriteCmd::new(
@@ -161,7 +161,7 @@ impl ProtocolHandler for JoyHubV2 {
       }
     }
 
-    let mut command_writer = self.last_cmds.try_write().expect("Locks should work");
+    let mut command_writer = self.last_cmds.write().expect("Locks should work");
     *command_writer = commands.to_vec();
     Ok(vec![HardwareWriteCmd::new(
       Endpoint::Tx,
