cf99bd5d6ba12231ae703b0c6245cd6bafd41714
Kyle Machulis <kyle@nonpolynomial.com>
2024-08-20 14:38:54 -0700

    chore: Remove unneeded TryFrom macros from derive library

--- buttplug_derive/src/lib.rs
+++ buttplug_derive/src/lib.rs
@@ -181,76 +181,6 @@ fn impl_buttplug_message_finalizer_macro(ast: &syn::DeriveInput) -> TokenStream 
   }
 }
 
-#[proc_macro_derive(TryFromButtplugClientMessage)]
-pub fn try_from_buttplug_client_message_derive(input: TokenStream) -> TokenStream {
-  // Construct a representation of Rust code as a syntax tree
-  // that we can manipulate
-  let ast = syn::parse(input).expect("Failure will cause compile failure.");
-
-  impl_try_from_buttplug_client_message_derive_macro(&ast)
-}
-
-fn impl_try_from_buttplug_client_message_derive_macro(ast: &syn::DeriveInput) -> TokenStream {
-  let name = &ast.ident;
-  if let syn::Data::Enum(ref e) = ast.data {
-    let idents: Vec<_> = e.variants.iter().map(|x| x.ident.clone()).collect();
-    let gen = quote! {
-        impl TryFrom<ButtplugClientMessage> for #name {
-            type Error = &'static str;
-
-            fn try_from(msg: ButtplugClientMessage) -> Result<Self, &'static str> {
-                match msg {
-                    #( ButtplugClientMessage::#idents(msg) => Ok(#name::#idents(msg)),)*
-                    _ => Err("ButtplugClientMessage cannot be converted to #name")
-                }
-            }
-        }
-
-        impl From<#name> for ButtplugClientMessage {
-            fn from(msg: #name) -> ButtplugClientMessage {
-                match msg {
-                    #( #name::#idents(msg) => ButtplugClientMessage::#idents(msg),)*
-                }
-            }
-        }
-    };
-    gen.into()
-  } else {
-    panic!("TryFromButtplugClientMessage only works on structs");
-  }
-}
-
-#[proc_macro_derive(TryFromButtplugServerMessage)]
-pub fn try_from_buttplug_out_message_derive(input: TokenStream) -> TokenStream {
-  // Construct a representation of Rust code as a syntax tree
-  // that we can manipulate
-  let ast = syn::parse(input).expect("Failure will cause compile failure.");
-
-  impl_try_from_buttplug_server_message_derive_macro(&ast)
-}
-
-fn impl_try_from_buttplug_server_message_derive_macro(ast: &syn::DeriveInput) -> TokenStream {
-  let name = &ast.ident;
-  if let syn::Data::Enum(ref e) = ast.data {
-    let idents: Vec<_> = e.variants.iter().map(|x| x.ident.clone()).collect();
-    let gen = quote! {
-        impl TryFrom<ButtplugServerMessage> for #name {
-            type Error = ButtplugMessageError;
-
-            fn try_from(msg: ButtplugServerMessage) -> Result<Self, ButtplugMessageError> {
-                match msg {
-                    #( ButtplugServerMessage::#idents(msg) => Ok(#name::#idents(msg.into())),)*
-                    _ => Err(ButtplugMessageError::MessageConversionError("ButtplugServerMessage cannot be converted to #name".to_owned()))
-                }
-            }
-        }
-    };
-    gen.into()
-  } else {
-    panic!("TryFromButtplugServerMessage only works on structs");
-  }
-}
-
 #[proc_macro_derive(FromSpecificButtplugMessage)]
 pub fn from_specific_buttplug_message_derive(input: TokenStream) -> TokenStream {
   // Construct a representation of Rust code as a syntax tree
@@ -286,40 +216,4 @@ fn impl_from_specific_buttplug_message_derive_macro(ast: &syn::DeriveInput) -> T
   } else {
     panic!("FromButtplugMessageUnion only works on structs");
   }
-}
-
-#[proc_macro_derive(ButtplugClientMessageType)]
-pub fn buttplug_client_message_type_derive(input: TokenStream) -> TokenStream {
-  // Construct a representation of Rust code as a syntax tree
-  // that we can manipulate
-  let ast = syn::parse(input).expect("Failure will cause compile failure.");
-
-  // Build the trait implementation
-  impl_buttplug_client_message_type_macro(&ast)
-}
-
-fn impl_buttplug_client_message_type_macro(ast: &syn::DeriveInput) -> TokenStream {
-  let name = &ast.ident;
-  let gen = quote! {
-      impl ButtplugClientMessageType for #name {}
-  };
-  gen.into()
-}
-
-#[proc_macro_derive(ButtplugServerMessageType)]
-pub fn buttplug_server_message_type_derive(input: TokenStream) -> TokenStream {
-  // Construct a representation of Rust code as a syntax tree
-  // that we can manipulate
-  let ast = syn::parse(input).expect("Failure will cause compile failure.");
-
-  // Build the trait implementation
-  impl_buttplug_server_message_type_macro(&ast)
-}
-
-fn impl_buttplug_server_message_type_macro(ast: &syn::DeriveInput) -> TokenStream {
-  let name = &ast.ident;
-  let gen = quote! {
-      impl ButtplugServerMessageType for #name {}
-  };
-  gen.into()
 }
