4049718e1649c27ca333c18b6927eddc88d9822d
blackspherefollower <darktech@black-sphere.co.uk>
2024-11-04 09:02:18 +0000

    feat: Adding support for Xuanhuan Masturbator

--- buttplug/tests/test_device_protocols.rs
+++ buttplug/tests/test_device_protocols.rs
@@ -112,6 +112,7 @@ async fn load_test_case(test_file: &str) -> DeviceTestCase {
 #[test_case("test_svakom_sam2.yaml" ; "Svakom Sam Neo 2 Pro")]
 #[test_case("test_feelingso.yaml" ; "FeelingSo Protocol")]
 #[test_case("test_deepsire.yaml" ; "DeepSire Protocol")]
+#[test_case("test_xuanhuan_protocol.yaml" ; "Xuanhuan Protocol")]
 #[tokio::test]
 async fn test_device_protocols_embedded_v3(test_file: &str) {
   //tracing_subscriber::fmt::init();
@@ -214,6 +215,7 @@ async fn test_device_protocols_embedded_v3(test_file: &str) {
 #[test_case("test_svakom_sam2.yaml" ; "Svakom Sam Neo 2 Pro")]
 #[test_case("test_feelingso.yaml" ; "FeelingSo Protocol")]
 #[test_case("test_deepsire.yaml" ; "DeepSire Protocol")]
+#[test_case("test_xuanhuan_protocol.yaml" ; "Xuanhuan Protocol")]
 #[tokio::test]
 async fn test_device_protocols_json_v3(test_file: &str) {
   //tracing_subscriber::fmt::init();
@@ -287,6 +289,7 @@ async fn test_device_protocols_json_v3(test_file: &str) {
 //#[test_case("test_cowgirl_cone_protocol.yaml" ; "The Cowgirl Cone Protocol")]
 #[test_case("test_amorelie_joy_protocol.yaml" ; "Amorelie Joy Protocol")]
 #[test_case("test_deepsire.yaml" ; "DeepSire Protocol")]
+#[test_case("test_xuanhuan_protocol.yaml" ; "Xuanhuan Protocol")]
 #[tokio::test]
 async fn test_device_protocols_embedded_v2(test_file: &str) {
   util::device_test::client::client_v2::run_embedded_test_case(&load_test_case(test_file).await)
@@ -359,6 +362,7 @@ async fn test_device_protocols_embedded_v2(test_file: &str) {
 //#[test_case("test_cowgirl_cone_protocol.yaml" ; "The Cowgirl Cone Protocol")]
 #[test_case("test_amorelie_joy_protocol.yaml" ; "Amorelie Joy Protocol")]
 #[test_case("test_deepsire.yaml" ; "DeepSire Protocol")]
+#[test_case("test_xuanhuan_protocol.yaml" ; "Xuanhuan Protocol")]
 #[tokio::test]
 async fn test_device_protocols_json_v2(test_file: &str) {
   util::device_test::client::client_v2::run_json_test_case(&load_test_case(test_file).await).await;
