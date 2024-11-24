fe75098ed657e4238a17da934ac7c2e0e5e3451c
blackspherefollower <darktech@black-sphere.co.uk>
2024-11-24 14:40:13 +0000

    chore: cargo fmt -- --emit=files

--- buttplug/src/server/device/protocol/serveu.rs
+++ buttplug/src/server/device/protocol/serveu.rs
@@ -12,27 +12,34 @@ use crate::{
     protocol::{generic_protocol_setup, ProtocolHandler},
   },
 };
-use std::sync::{Arc, atomic::{AtomicU8, Ordering}};
+use std::sync::{
+  atomic::{AtomicU8, Ordering},
+  Arc,
+};
 
 generic_protocol_setup!(ServeU, "serveu");
 
 #[derive(Default)]
 pub struct ServeU {
-  last_position: Arc<AtomicU8>
+  last_position: Arc<AtomicU8>,
 }
 
 impl ProtocolHandler for ServeU {
-
   fn handle_linear_cmd(
-      &self,
-      message: crate::core::message::LinearCmdV4,
-    ) -> Result<Vec<HardwareCommand>, ButtplugDeviceError> {
+    &self,
+    message: crate::core::message::LinearCmdV4,
+  ) -> Result<Vec<HardwareCommand>, ButtplugDeviceError> {
     let last_pos = self.last_position.load(Ordering::Relaxed);
-    let current_cmd = message.vectors().first().ok_or(ButtplugDeviceError::DeviceFeatureCountMismatch(1, 0))?;
+    let current_cmd = message
+      .vectors()
+      .first()
+      .ok_or(ButtplugDeviceError::DeviceFeatureCountMismatch(1, 0))?;
     // Need to get "units" (abstracted steps 0-100) per second, so calculate how far we need to move over our goal duration.
-    let goal_pos =(current_cmd.position() * 100f64).ceil() as u8;
+    let goal_pos = (current_cmd.position() * 100f64).ceil() as u8;
     self.last_position.store(goal_pos, Ordering::Relaxed);
-    let speed_threshold = ((((goal_pos as i8) - last_pos as i8).abs()) as f64 / ((current_cmd.duration() as f64) / 1000f64)).ceil();
+    let speed_threshold = ((((goal_pos as i8) - last_pos as i8).abs()) as f64
+      / ((current_cmd.duration() as f64) / 1000f64))
+      .ceil();
 
     let speed = if speed_threshold <= 0.00001 {
       // Stop device
