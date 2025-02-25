385d84db1ad839a6f49a01795a3ebb8396a80836
blackspherefollower <darktech@black-sphere.co.uk>
2025-02-25 09:16:49 +0000

    chore: cargo fmt

--- buttplug/src/server/device/protocol/tryfun_meta2.rs
+++ buttplug/src/server/device/protocol/tryfun_meta2.rs
@@ -5,7 +5,6 @@
 // Licensed under the BSD 3-Clause license. See LICENSE file in the project root
 // for full license information.
 
-use std::sync::atomic::{AtomicU8, Ordering};
 use crate::{
   core::{errors::ButtplugDeviceError, message::Endpoint},
   generic_protocol_setup,
@@ -14,6 +13,7 @@ use crate::{
     protocol::ProtocolHandler,
   },
 };
+use std::sync::atomic::{AtomicU8, Ordering};
 
 generic_protocol_setup!(TryFunMeta2, "tryfun-meta2");
 
@@ -33,7 +33,16 @@ impl ProtocolHandler for TryFunMeta2 {
     scalar: u32,
   ) -> Result<Vec<HardwareCommand>, ButtplugDeviceError> {
     let mut sum: u8 = 0xff;
-    let mut data = vec![self.packet_id.fetch_add(1, Ordering::Relaxed), 0x02, 0x00, 0x05, 0x21, 0x05, 0x0b, scalar as u8];
+    let mut data = vec![
+      self.packet_id.fetch_add(1, Ordering::Relaxed),
+      0x02,
+      0x00,
+      0x05,
+      0x21,
+      0x05,
+      0x0b,
+      scalar as u8,
+    ];
     let mut count = 1;
     for item in data.iter().skip(1) {
       sum -= item;
@@ -45,7 +54,10 @@ impl ProtocolHandler for TryFunMeta2 {
     Ok(vec![HardwareWriteCmd::new(Endpoint::Tx, data, false).into()])
   }
 
-  fn handle_rotate_cmd(&self, commands: &[Option<(u32, bool)>]) -> Result<Vec<HardwareCommand>, ButtplugDeviceError> {
+  fn handle_rotate_cmd(
+    &self,
+    commands: &[Option<(u32, bool)>],
+  ) -> Result<Vec<HardwareCommand>, ButtplugDeviceError> {
     if commands.len() >= 1 {
       if let Some(cmd) = commands[0] {
         let mut speed = cmd.0 as i8;
@@ -54,7 +66,16 @@ impl ProtocolHandler for TryFunMeta2 {
           speed *= -1;
         }
         let mut sum: u8 = 0xff;
-        let mut data = vec![self.packet_id.fetch_add(1, Ordering::Relaxed), 0x02, 0x00, 0x05, 0x21, 0x05, 0x0e, speed as u8];
+        let mut data = vec![
+          self.packet_id.fetch_add(1, Ordering::Relaxed),
+          0x02,
+          0x00,
+          0x05,
+          0x21,
+          0x05,
+          0x0e,
+          speed as u8,
+        ];
         let mut count = 1;
         for item in data.iter().skip(1) {
           sum -= item;
@@ -75,7 +96,16 @@ impl ProtocolHandler for TryFunMeta2 {
     scalar: u32,
   ) -> Result<Vec<HardwareCommand>, ButtplugDeviceError> {
     let mut sum: u8 = 0xff;
-    let mut data = vec![self.packet_id.fetch_add(1, Ordering::Relaxed), 0x02, 0x00, 0x05, 0x21, 0x05, 0x08, scalar as u8];
+    let mut data = vec![
+      self.packet_id.fetch_add(1, Ordering::Relaxed),
+      0x02,
+      0x00,
+      0x05,
+      0x21,
+      0x05,
+      0x08,
+      scalar as u8,
+    ];
     let mut count = 1;
     for item in data.iter().skip(1) {
       sum -= item;
