fc7f6f5b03fbb953a62da35b574ad2907e3bb1bb
Kyle Machulis <kyle@nonpolynomial.com>
2025-05-26 19:05:49 -0700

    chore: Add value-position to TCode impl

--- buttplug/src/server/device/protocol/tcode_v03.rs
+++ buttplug/src/server/device/protocol/tcode_v03.rs
@@ -22,14 +22,27 @@ generic_protocol_setup!(TCodeV03, "tcode-v03");
 pub struct TCodeV03 {}
 
 impl ProtocolHandler for TCodeV03 {
+
+  fn handle_value_position_cmd(
+      &self,
+      cmd: &CheckedValueCmdV4,
+    ) -> Result<Vec<HardwareCommand>, ButtplugDeviceError> {
+    let mut msg_vec = vec![];
+
+    let command = format!("L0{:03}\n", cmd.value());
+    msg_vec.push(HardwareWriteCmd::new(cmd.feature_id(), Endpoint::Tx, command.as_bytes().to_vec(), false).into());
+
+    Ok(msg_vec)
+    
+  }
+
   fn handle_position_with_duration_cmd(
     &self,
     cmd: &CheckedValueWithParameterCmdV4,
   ) -> Result<Vec<HardwareCommand>, ButtplugDeviceError> {
     let mut msg_vec = vec![];
-    let position = cmd.value() as u32;
-
-    let command = format!("L{}{:02}I{}\n", cmd.feature_index(), position, cmd.parameter() as u32);
+    
+    let command = format!("L{}{:03}I{}\n", cmd.feature_index(), cmd.value(), cmd.parameter() as u32);
     msg_vec.push(HardwareWriteCmd::new(cmd.feature_id(), Endpoint::Tx, command.as_bytes().to_vec(), false).into());
 
     Ok(msg_vec)
