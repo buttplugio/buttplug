6efc0c31ec7952eae40e6615f5fd3b1d2a32a147
Kyle Machulis <kyle@nonpolynomial.com>
2025-06-20 21:22:31 -0700

    chore: Fix svakom iker protocol impl

--- buttplug/src/server/device/protocol/svakom/svakom_iker.rs
+++ buttplug/src/server/device/protocol/svakom/svakom_iker.rs
@@ -6,22 +6,17 @@
 // for full license information.
 
 use crate::{
-  core::{
-    errors::ButtplugDeviceError,
-    message::Endpoint,
-  }, generic_protocol_setup, server::device::{
+  core::{errors::ButtplugDeviceError, message::Endpoint},
+  generic_protocol_setup,
+  server::device::{
     hardware::{HardwareCommand, HardwareWriteCmd},
-    protocol::{
-      ProtocolHandler,
-      ProtocolKeepaliveStrategy,
-    },
-  }
+    protocol::{ProtocolHandler, ProtocolKeepaliveStrategy},
+  },
 };
 use std::sync::atomic::{AtomicU8, Ordering};
 use std::sync::Arc;
 
 generic_protocol_setup!(SvakomIker, "svakom-iker");
-
 
 #[derive(Default)]
 pub struct SvakomIker {
@@ -34,80 +29,41 @@ impl ProtocolHandler for SvakomIker {
   }
 
   fn handle_output_vibrate_cmd(
-      &self,
-      feature_index: u32,
-      feature_id: uuid::Uuid,
-      speed: u32,
-    ) -> Result<Vec<HardwareCommand>, ButtplugDeviceError> {
-    if feature_index == 0 {
+    &self,
+    feature_index: u32,
+    feature_id: uuid::Uuid,
+    speed: u32,
+  ) -> Result<Vec<HardwareCommand>, ButtplugDeviceError> {
+    self.last_speeds[feature_index as usize].store(speed as u8, Ordering::Relaxed);
+    let vibe0 = self.last_speeds[0].load(Ordering::Relaxed);
+    let vibe1 = self.last_speeds[1].load(Ordering::Relaxed);
+    if vibe0 == 0 && vibe1 == 0 {
       Ok(vec![HardwareWriteCmd::new(
         feature_id,
         Endpoint::Tx,
-        [0x55, 0x03, 0x03, 0x00, 0x01, speed as u8].to_vec(),
+        [0x55, 0x07, 0x00, 0x00, 0x00, 0x00].to_vec(),
         false,
       )
       .into()])
     } else {
-      Ok(vec![HardwareWriteCmd::new(
+      let mut msgs = vec!();
+      msgs.push(HardwareWriteCmd::new(
         feature_id,
         Endpoint::Tx,
-        [0x55, 0x07, 0x07, 0x00, 0x01, speed as u8].to_vec(),
+        [0x55, 0x03, 0x03, 0x00, 0x01, vibe0 as u8].to_vec(),
         false,
       )
-      .into()])
-    }
-    /*
-    self.last_speeds[feature_index as usize].store(speed as u8, Ordering::Relaxed);
-    let mut msg_vec = vec![];
-    let speed0 = self.last_speeds[0].load(Ordering::Relaxed);
-    let speed1 = self.last_speeds[1].load(Ordering::Relaxed);
-    let vibe_off = speed0 == 0 && speed1 == 0;
-
-    if let Some((_, speed)) = cmds[0] {
-      self.last_speeds[0].store(speed as u8, Ordering::Relaxed);
-      if speed == 0 {
-        vibe_off = true;
-      }
-      msg_vec.push(
-        HardwareWriteCmd::new(
+      .into());
+      if vibe1 > 0 {
+        msgs.push(HardwareWriteCmd::new(
+          feature_id,
           Endpoint::Tx,
-          [0x55, 0x03, 0x03, 0x00, 0x01, speed as u8].to_vec(),
+          [0x55, 0x07, 0x00, 0x00, vibe1 as u8, 0x00].to_vec(),
           false,
         )
-        .into(),
-      );
-    }
-    if cmds.len() > 1 {
-      if let Some((_, speed)) = cmds[1] {
-        self.last_speeds[1].store(speed as u8, Ordering::Relaxed);
-        msg_vec.push(
-          HardwareWriteCmd::new(
-            Endpoint::Tx,
-            [0x55, 0x07, 0x00, 0x00, speed as u8, 0x00].to_vec(),
-            false,
-          )
-          .into(),
-        );
-      } else if vibe_off && self.last_speeds[1].load(Ordering::Relaxed) != 0 {
-        msg_vec.push(
-          HardwareWriteCmd::new(
-            Endpoint::Tx,
-            [
-              0x55,
-              0x07,
-              0x00,
-              0x00,
-              self.last_speeds[1].load(Ordering::Relaxed),
-              0x00,
-            ]
-            .to_vec(),
-            false,
-          )
-          .into(),
-        );
+        .into());
       }
+      Ok(msgs)
     }
-    Ok(msg_vec)
-    */
   }
 }
