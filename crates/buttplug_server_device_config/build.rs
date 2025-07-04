0e40e585d69defd9fe1fd8638dca9d7a14aeee55
Kyle Machulis <kyle@nonpolynomial.com>
2025-07-04 16:36:58 -0700

    build: Check for changes between old/new device files on build
    Just in case
    
    Fixes #747

--- crates/buttplug_server_device_config/build.rs
+++ crates/buttplug_server_device_config/build.rs
@@ -10,18 +10,18 @@ const OUTPUT_FILE: &str = "./build-config/buttplug-device-config-v4.json";
 const PROTOCOL_DIR: &str = "./device-config-v4/protocols/";
 const SCHEMA_FILE: &str = "./device-config-v4/buttplug-device-config-schema-v4.json";
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Eq, PartialEq)]
 struct VersionFile {
   version: BuildVersion
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Eq, PartialEq, Clone, Copy)]
 struct BuildVersion {
   pub major: u32,
   pub minor: u32
 }
 
-#[derive(Serialize)]
+#[derive(Deserialize, Serialize, Eq, PartialEq)]
 struct JsonOutputFile {
   version: BuildVersion,
   protocols: BTreeMap<String, Value>
@@ -34,8 +34,7 @@ fn main() {
   let mut version: VersionFile = serde_yaml::from_str(&std::fs::read_to_string(VERSION_FILE).unwrap()).unwrap();
   // Bump minor version
   version.version.minor += 1;
-  std::fs::write(VERSION_FILE, serde_yaml::to_string(&version).unwrap().as_bytes()).unwrap();
-
+  
   // Compile device config file
   let mut output = JsonOutputFile {
     // lol
@@ -49,14 +48,21 @@ fn main() {
   }
 
   let json = serde_json::to_string(&output).unwrap();
+
+  // Validate
   let validator = JSONValidator::new(&std::fs::read_to_string(SCHEMA_FILE).unwrap());
   validator.validate(&json).unwrap();
 
-  // Validate
+  // See if it's actually different than our last output file
+  if let Ok(true) = std::fs::exists(OUTPUT_FILE) {
+    let old_output: JsonOutputFile = serde_json::from_str(&std::fs::read_to_string(OUTPUT_FILE).unwrap()).unwrap();
+    if old_output.protocols == output.protocols {
+      // No actual changes, break out early, don't save
+      return;
+    }
+  }
 
   // Save it to the build_config directory
-  std::fs::write(OUTPUT_FILE, json.as_bytes()).unwrap();
-
-  
-
+ std::fs::write(VERSION_FILE, serde_yaml::to_string(&version).unwrap().as_bytes()).unwrap();
+ std::fs::write(OUTPUT_FILE, json.as_bytes()).unwrap();
 }
