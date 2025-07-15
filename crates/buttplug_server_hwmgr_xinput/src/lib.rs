5d55dbb0de1f8e5195447790e042bbf47e2e39b7
Kyle Machulis <kyle@nonpolynomial.com>
2025-07-14 19:56:12 -0700

    build: Don't build xinput outside of windows

--- crates/buttplug_server_hwmgr_xinput/src/lib.rs
+++ crates/buttplug_server_hwmgr_xinput/src/lib.rs
@@ -5,15 +5,20 @@
 // Licensed under the BSD 3-Clause license. See LICENSE file in the project root
 // for full license information.
 
+#[cfg(target_os = "windows")]
 #[macro_use]
 extern crate log;
 
+#[cfg(target_os = "windows")]
 #[macro_use]
 extern crate strum_macros;
 
+#[cfg(target_os = "windows")]
 mod xinput_device_comm_manager;
+#[cfg(target_os = "windows")]
 mod xinput_hardware;
 
+#[cfg(target_os = "windows")]
 pub use xinput_device_comm_manager::{
   XInputDeviceCommunicationManager,
   XInputDeviceCommunicationManagerBuilder,
