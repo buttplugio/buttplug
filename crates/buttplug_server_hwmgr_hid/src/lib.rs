4d0cb438bfb48a42687d8bb072ee853d019f844b
Kyle Machulis <kyle@nonpolynomial.com>
2026-01-17 13:00:17 -0800

    chore: Only import desktop hardware on desktop

--- crates/buttplug_server_hwmgr_hid/src/lib.rs
+++ crates/buttplug_server_hwmgr_hid/src/lib.rs
@@ -1,8 +1,12 @@
 #[macro_use]
 extern crate log;
 
+#[cfg(any(target_os = "windows", target_os = "macos", target_os = "linux"))]
 pub mod hid_comm_manager;
+#[cfg(any(target_os = "windows", target_os = "macos", target_os = "linux"))]
 pub mod hid_device_impl;
+#[cfg(any(target_os = "windows", target_os = "macos", target_os = "linux"))]
 mod hidapi_async;
 
+#[cfg(any(target_os = "windows", target_os = "macos", target_os = "linux"))]
 pub use hid_comm_manager::{HidCommunicationManager, HidCommunicationManagerBuilder};
--- crates/buttplug_server_hwmgr_lovense_dongle/src/lib.rs
+++ crates/buttplug_server_hwmgr_lovense_dongle/src/lib.rs
@@ -8,12 +8,18 @@
 #[macro_use]
 extern crate log;
 
+#[cfg(any(target_os = "windows", target_os = "macos", target_os = "linux"))]
 pub mod lovense_dongle_hardware;
+#[cfg(any(target_os = "windows", target_os = "macos", target_os = "linux"))]
 mod lovense_dongle_messages;
+#[cfg(any(target_os = "windows", target_os = "macos", target_os = "linux"))]
 mod lovense_dongle_state_machine;
+#[cfg(any(target_os = "windows", target_os = "macos", target_os = "linux"))]
 pub mod lovense_hid_dongle_comm_manager;
 
+#[cfg(any(target_os = "windows", target_os = "macos", target_os = "linux"))]
 pub use lovense_dongle_hardware::{LovenseDongleHardware, LovenseDongleHardwareConnector};
+#[cfg(any(target_os = "windows", target_os = "macos", target_os = "linux"))]
 pub use lovense_hid_dongle_comm_manager::{
   LovenseHIDDongleCommunicationManager,
   LovenseHIDDongleCommunicationManagerBuilder,
