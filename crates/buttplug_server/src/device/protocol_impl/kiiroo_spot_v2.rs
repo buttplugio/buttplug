6863fbc826f3bf4f735397c56f52526eee269b1a
blackspherefollower <darktech@black-sphere.co.uk>
2026-07-03 17:06:31 +0100

    feat: Adding support for Kiiroo Keon 2 and Spot 2

---
+++ crates/buttplug_server/src/device/protocol_impl/kiiroo_spot_v2.rs
@@ -0,0 +1,67 @@
+// Buttplug Rust Source Code File - See https://buttplug.io for more info.
+//
+// Copyright 2016-2026 Nonpolynomial Labs LLC. All rights reserved.
+//
+// Licensed under the BSD 3-Clause license. See LICENSE file in the project root
+// for full license information.
+
+use crate::device::{
+  hardware::{Hardware, HardwareCommand, HardwareReadCmd, HardwareWriteCmd},
+  protocol::{ProtocolHandler, generic_protocol_setup},
+};
+use buttplug_core::{
+  errors::ButtplugDeviceError,
+  message::{self, InputReadingV4, InputTypeReading, InputValue},
+};
+use buttplug_server_device_config::Endpoint;
+use futures::{FutureExt, future::BoxFuture};
+use std::{default::Default, sync::Arc};
+use uuid::Uuid;
+
+generic_protocol_setup!(KiirooSpotV2, "kiiroo-spot-v2");
+
+#[derive(Default)]
+pub struct KiirooSpotV2 {}
+
+impl ProtocolHandler for KiirooSpotV2 {
+  fn handle_output_vibrate_cmd(
+    &self,
+    _feature_index: u32,
+    feature_id: Uuid,
+    speed: u32,
+  ) -> Result<Vec<HardwareCommand>, ButtplugDeviceError> {
+    Ok(vec![
+      HardwareWriteCmd::new(
+        &[feature_id],
+        Endpoint::Tx,
+        vec![0x01, speed as u8, speed as u8, 0x03, 0xe8, 0xff],
+        true,
+      )
+      .into(),
+    ])
+  }
+
+  fn handle_battery_level_cmd(
+    &self,
+    device_index: u32,
+    device: Arc<Hardware>,
+    feature_index: u32,
+    feature_id: Uuid,
+  ) -> BoxFuture<'_, Result<InputReadingV4, ButtplugDeviceError>> {
+    debug!("Trying to get battery reading.");
+    let msg = HardwareReadCmd::new(feature_id, Endpoint::RxBLEBattery, 20, 0);
+    let fut = device.read_value(&msg);
+    async move {
+      let hw_msg = fut.await?;
+      let data = hw_msg.data();
+      let battery_reading = message::InputReadingV4::new(
+        device_index,
+        feature_index,
+        InputTypeReading::Battery(InputValue::new(data[0])),
+      );
+      debug!("Got battery reading: {}", data[0]);
+      Ok(battery_reading)
+    }
+    .boxed()
+  }
+}
