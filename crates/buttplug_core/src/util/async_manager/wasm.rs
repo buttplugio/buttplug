9741fec6fc2e0bd4e5d8dc2b2d9a018b963f2083
Kyle Machulis <kyle@nonpolynomial.com>
2026-07-27 19:58:49 -0700

    core: add owner-local task lifecycle

--- crates/buttplug_core/src/util/async_manager/wasm.rs
+++ crates/buttplug_core/src/util/async_manager/wasm.rs
@@ -5,7 +5,8 @@
 // Licensed under the BSD 3-Clause license. See LICENSE file in the project root
 // for full license information.
 
-use futures::{future::BoxFuture, task::LocalFutureObj};
+use super::{TaskCompletion, TaskCompletionResult};
+use futures::{channel::oneshot, future::BoxFuture, task::LocalFutureObj};
 use std::time::Duration;
 use tracing::{Instrument, Span};
 
@@ -13,8 +14,19 @@ use tracing::{Instrument, Span};
 pub struct WasmBindgenAsyncManager {}
 
 impl super::AsyncManager for WasmBindgenAsyncManager {
-  fn spawn(&self, future: LocalFutureObj<'static, ()>, span: Span) {
-    wasm_bindgen_futures::spawn_local(future.instrument(span));
+  fn spawn(
+    &self,
+    future: LocalFutureObj<'static, TaskCompletionResult>,
+    span: Span,
+  ) -> TaskCompletion {
+    let (sender, receiver) = oneshot::channel();
+    wasm_bindgen_futures::spawn_local(
+      async move {
+        let _ = sender.send(future.await);
+      }
+      .instrument(span),
+    );
+    Box::pin(async move { receiver.await.unwrap_or(TaskCompletionResult::Cancelled) })
   }
 
   fn sleep(&self, duration: Duration) -> BoxFuture<'static, ()> {
